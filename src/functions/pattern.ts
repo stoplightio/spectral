770d75ad6d09fec7ec9eb58f1edc91c22b26b222
casserni <nicholascassera@gmail.com>
2018-10-08 17:02:28 -0500

    import should from as-function

--- src/functions/pattern.ts
+++ src/functions/pattern.ts
@@ -1,7 +1,8 @@
 import { IPatternRule, IRuleFunction, IRuleResult } from '../types';
 import { ensureRule } from './utils/ensureRule';
 
-import * as should from 'should';
+// @ts-ignore
+import * as should from 'should/as-function';
 
 export const pattern: IRuleFunction<IPatternRule> = (object, r, ruleMeta) => {
   const results: IRuleResult[] = [];
