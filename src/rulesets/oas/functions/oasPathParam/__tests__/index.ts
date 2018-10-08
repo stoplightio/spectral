86107d6effd49c66146fc107312dc9b7eaba1bbc
Ross McDonald <ross@stoplight.io>
2018-10-08 11:37:11 -0500

    Update tests for new directory layout

--- src/rulesets/oas/functions/oasPathParam/__tests__/index.ts
+++ src/rulesets/oas/functions/oasPathParam/__tests__/index.ts
@@ -4,23 +4,23 @@ import { commonOasRuleset } from '../../../index';
 const ruleset = commonOasRuleset();
 
 describe('oasPathParam', () => {
-  test('pathParam', () => {
-    const s = new Spectral({
-      rulesets: [
-        {
-          functions: ruleset.functions,
-          rules: {
-            oas2: {
-              'path-params': Object.assign(ruleset.rules['oas2|oas3']['path-params'], {
-                enabled: true,
-              }),
-            },
+  const s = new Spectral({
+    rulesets: [
+      {
+        functions: ruleset.functions,
+        rules: {
+          oas2: {
+            'path-params': Object.assign(ruleset.rules['oas2|oas3']['path-params'], {
+              enabled: true,
+            }),
           },
         },
-      ],
-    });
+      },
+    ],
+  });
 
-    let results = s.run({
+  test('No error if templated path is not used', () => {
+    const results = s.run({
       spec: 'oas2',
       target: {
         paths: {
@@ -31,8 +31,10 @@ describe('oasPathParam', () => {
       },
     });
     expect(results.length).toEqual(0);
+  });
 
-    results = s.run({
+  test('Error if no path parameter definition', () => {
+    const results = s.run({
       spec: 'oas2',
       target: {
         paths: {
@@ -43,5 +45,97 @@ describe('oasPathParam', () => {
       },
     });
     expect(results.length).toEqual(1);
+    expect(results[0].path).toEqual(['$', 'paths', '/foo/{bar}']);
+    expect(results[0].message).toContain('bar');
+  });
+
+  test('No error if path parameter definition is used (at the path level)', () => {
+    const results = s.run({
+      spec: 'oas2',
+      target: {
+        paths: {
+          '/foo/{bar}': {
+            parameters: [
+              {
+                name: 'bar',
+                in: 'path',
+              },
+            ],
+            get: {},
+          },
+        },
+      },
+    });
+    expect(results.length).toEqual(0);
+  });
+
+  test('No error if path parameter definition is set (at the operation level)', () => {
+    const results = s.run({
+      spec: 'oas2',
+      target: {
+        paths: {
+          '/foo/{bar}': {
+            get: {
+              parameters: [
+                {
+                  name: 'bar',
+                  in: 'path',
+                },
+              ],
+            },
+          },
+        },
+      },
+    });
+    expect(results.length).toEqual(0);
+  });
+
+  test('Error if duplicate path parameter definitions are specified', () => {
+    const results = s.run({
+      spec: 'oas2',
+      target: {
+        paths: {
+          '/foo/{bar}': {
+            parameters: [
+              {
+                name: 'bar',
+                in: 'path',
+              },
+            ],
+            get: {
+              parameters: [
+                {
+                  name: 'bar',
+                  in: 'path',
+                },
+              ],
+            },
+          },
+        },
+      },
+    });
+    expect(results.length).toEqual(1);
+    expect(results[0].message).toContain('bar');
+  });
+
+  test('Error if duplicate path parameters with same name are used', () => {
+    const results = s.run({
+      spec: 'oas2',
+      target: {
+        paths: {
+          '/foo/{bar}/{bar}': {
+            parameters: [
+              {
+                name: 'bar',
+                in: 'path',
+              },
+            ],
+            get: {},
+          },
+        },
+      },
+    });
+    expect(results.length).toEqual(1);
+    expect(results[0].message).toContain('bar');
   });
 });
