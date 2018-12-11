faf84c06ec3b0ca45e70b0ab538c3f20abc2ec51
casserni <nicholascassera@gmail.com>
2018-12-10 18:17:37 -0600

    fix(security): add back oasOpSecurityDefined

--- src/rulesets/oas/index.ts
+++ src/rulesets/oas/index.ts
@@ -7,7 +7,7 @@ export const commonOasFunctions = (): FunctionCollection => {
   return {
     oasPathParam: require('./functions/oasPathParam').oasPathParam,
     oasOp2xxResponse: require('./functions/oasOp2xxResponse').oasOp2xxResponse,
-    // oasOpSecurityDefined: require('./functions/oasOpSecurityDefined').oasOpSecurityDefined,
+    oasOpSecurityDefined: require('./functions/oasOpSecurityDefined').oasOpSecurityDefined, // used in oas2/oas3 differently see their rulesets for details
     oasOpIdUnique: require('./functions/oasOpIdUnique').oasOpIdUnique,
     oasOpFormDataConsumeCheck: require('./functions/oasOpFormDataConsumeCheck').oasOpFormDataConsumeCheck,
     oasOpParams: require('./functions/oasOpParams').oasOpParams,
