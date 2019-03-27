15a89f1e61b4c1fcbb01713d09de40dbf4d59cc2
Phil Sturgeon <me@philsturgeon.uk>
2019-03-27 16:25:20 +0100

    OAS : Ignore anything at PathItem level which is not HTTP verb (#111)
    * test(operation-2xx-response): trying to confirm if this rule works
    
    Struggling to figure out if this rule actually works or not, and tests are giving a warning.ç
    
    re #109
    
    * test: operation-2xx-response
    
    * bug: show new JSONPath ignores x- path items

---
+++ src/rulesets/oas/__tests__/operation-2xx-response.ts
@@ -0,0 +1,110 @@
+import { commonOasFunctions, commonOasRules } from '../';
+import { Spectral } from '../../../spectral';
+
+const rules = commonOasRules();
+const { oasOp2xxResponse } = commonOasFunctions();
+
+describe('operation-2xx-response', () => {
+  let spectral: Spectral;
+
+  beforeEach(() => {
+    spectral = new Spectral();
+    spectral.addFunctions({ oasOp2xxResponse });
+    spectral.addRules({
+      'operation-2xx-response': rules['operation-2xx-response'],
+    });
+  });
+
+  test('is happy when a 200 response is set', async () => {
+    const results = await spectral.run({
+      swagger: '2.0',
+      paths: {
+        '/path': {
+          get: {
+            responses: {
+              '200': {},
+            },
+          },
+        },
+      },
+    });
+
+    expect(results).toHaveLength(0);
+  });
+
+  test('is happy when a (non 200) 2xx response is set', async () => {
+    const results = await spectral.run({
+      swagger: '2.0',
+      paths: {
+        '/path': {
+          get: {
+            responses: {
+              '204': {},
+            },
+          },
+        },
+      },
+    });
+
+    expect(results).toHaveLength(0);
+  });
+
+  ['put', 'post', 'delete', 'options', 'head', 'patch', 'trace'].forEach(method => {
+    test(`warns if HTTP verb ${method} is missing a 2xx response`, async () => {
+      const obj = {
+        swagger: '2.0',
+        paths: {
+          '/path': {},
+        },
+      };
+      obj.paths['/path'][method] = {
+        responses: {
+          '418': {},
+        },
+      };
+      const results = await spectral.run(obj);
+      expect(results).toEqual([
+        expect.objectContaining({
+          code: 'operation-2xx-response',
+          message: 'operations must define at least one 2xx response',
+          path: ['paths', '/path', method, 'responses'],
+        }),
+      ]);
+    });
+  });
+
+  test('warns about missing 2xx response', async () => {
+    const results = await spectral.run({
+      swagger: '2.0',
+      paths: {
+        '/path': {
+          get: {
+            responses: {
+              400: {},
+            },
+          },
+        },
+      },
+    });
+
+    expect(results).toEqual([
+      expect.objectContaining({
+        code: 'operation-2xx-response',
+        message: 'operations must define at least one 2xx response',
+        path: ['paths', '/path', 'get', 'responses'],
+      }),
+    ]);
+  });
+
+  test('ignores any at the PathItem level which is not a HTTP verb', async () => {
+    const results = await spectral.run({
+      swagger: '2.0',
+      paths: {
+        '/path': {
+          'x-summary': 'why is this here',
+        },
+      },
+    });
+    expect(results).toEqual([]);
+  });
+});
