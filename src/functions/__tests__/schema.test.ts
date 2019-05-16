1b6c1c6de4aa3748a34861c41be71294f83db46d
Phil Sturgeon <me@philsturgeon.uk>
2019-05-16 15:44:36 +0200

    fix: ignore openapi-only formats to avoid errors
    This is a temporary workaround as currently our AJV validation is not aware of OpenAPI specific formats like int32 and int64. See #187 for more information.

--- src/functions/__tests__/schema.test.ts
+++ src/functions/__tests__/schema.test.ts
@@ -5,39 +5,67 @@ function runSchema(target: any, schemaObj: object) {
 }
 
 describe('schema', () => {
-  const testSchema = {
-    type: 'array',
-    items: {
-      type: 'string',
-    },
-    maxItems: 1,
-  };
+  describe('when schema defines a simple array', () => {
+    const testSchema = {
+      type: 'array',
+      items: {
+        type: 'string',
+      },
+      maxItems: 1,
+    };
 
-  test('will error with totally invalid input', () => {
-    const input = { foo: 'bar' };
-    expect(runSchema(input, testSchema)).toMatchInlineSnapshot(`
-Array [
-  Object {
-    "message": "should be array",
-    "path": Array [
-      "$",
-    ],
-  },
-]
-`);
+    test('errors with totally invalid input', () => {
+      const input = { foo: 'bar' };
+      expect(runSchema(input, testSchema)).toEqual([
+        expect.objectContaining({
+          message: 'should be array',
+          path: ['$'],
+        }),
+      ]);
+    });
+
+    test('errors with subtly invalid input', () => {
+      const input = ['1', '2'];
+      expect(runSchema(input, testSchema)).toEqual([
+        expect.objectContaining({
+          message: 'should NOT have more than 1 items',
+          path: ['$'],
+        }),
+      ]);
+    });
   });
 
-  test('will error with subtly invalid input', () => {
-    const input = ['1', '2'];
-    expect(runSchema(input, testSchema)).toMatchInlineSnapshot(`
-Array [
-  Object {
-    "message": "should NOT have more than 1 items",
-    "path": Array [
-      "$",
-    ],
-  },
-]
-`);
+  describe('when schema defines common formats', () => {
+    const testSchema = {
+      type: 'string',
+      format: 'email',
+    };
+
+    test('errors for not emails', () => {
+      const input = 'not an email';
+      expect(runSchema(input, testSchema)).toEqual([
+        expect.objectContaining({
+          message: 'should match format "email"',
+          path: ['$'],
+        }),
+      ]);
+    });
+
+    test('considers emails valid', () => {
+      const input = 'email@example.com';
+      expect(runSchema(input, testSchema)).toEqual([]);
+    });
+  });
+
+  describe('when schema defines OpenAPI specific formats', () => {
+    const testSchema = {
+      type: 'number',
+      format: 'int32',
+    };
+
+    test('accepts a number of any format', () => {
+      const input = 123;
+      expect(runSchema(input, testSchema)).toEqual([]);
+    });
   });
 });
