7bbf087c044705451c90d47c6ed8d1c1d0143956
Jakub Rożek <jakub@stoplight.io>
2019-05-17 20:12:49 +0200

    test: improve tests of formats

--- src/rulesets/oas3/__tests__/valid-example.ts
+++ src/rulesets/oas3/__tests__/valid-example.ts
@@ -3,6 +3,8 @@ import { oas3Rules } from '../index';
 
 const ruleset = { rules: oas3Rules() };
 
+declare var test: jest.It;
+
 describe('valid-example', () => {
   const s = new Spectral();
   s.addRules({
@@ -167,25 +169,48 @@ Array [
     expect(results).toEqual([]);
   });
 
-  test('does report invalid int64', async () => {
-    const results = await s.run({
-      xoxo: {
-        type: 'object',
-        properties: {
-          ip_address: {
-            type: 'integer',
-            format: 'int64',
-            example: Number.MAX_VALUE,
+  test.each([['byte', '1'], ['int32', 2 ** 31], ['int64', 2 ** 63], ['float', 2 ** 128]])(
+    'reports invalid usage of %s format',
+    async (format, example) => {
+      const results = await s.run({
+        xoxo: {
+          type: 'object',
+          properties: {
+            ip_address: {
+              type: ['string', 'number'],
+              format,
+              example,
+            },
           },
         },
-      },
-    });
+      });
 
-    expect(results).toEqual([
-      expect.objectContaining({
-        code: 'valid-example',
-        message: 'should match format "int64"',
-      }),
-    ]);
-  });
+      expect(results).toEqual([
+        expect.objectContaining({
+          code: 'valid-example',
+          message: `should match format "${format}"`,
+        }),
+      ]);
+    },
+  );
+
+  test.each([['byte', 'MTI3'], ['int32', 2 ** 30], ['int64', 2 ** 40], ['float', 2 ** 64], ['double', 2 ** 1028]])(
+    'does not report valid usage of %s format',
+    async (format, example) => {
+      const results = await s.run({
+        xoxo: {
+          type: 'object',
+          properties: {
+            ip_address: {
+              type: ['string', 'number'],
+              format,
+              example,
+            },
+          },
+        },
+      });
+
+      expect(results).toHaveLength(0);
+    },
+  );
 });
