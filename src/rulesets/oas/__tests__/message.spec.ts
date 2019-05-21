313bd7ca47485b6756b613bc0c2a8eb536569f28
Jakub Rożek <jakub@stoplight.io>
2019-05-21 13:34:08 +0200

    feat: change str interpolation impl

--- src/rulesets/oas/__tests__/message.spec.ts
+++ src/rulesets/oas/__tests__/message.spec.ts
@@ -2,12 +2,32 @@ import { message } from '../../message';
 
 describe('message util', () => {
   test('interpolates correctly', () => {
-    const val = message`oops... ${'property'} missing;error: ${'error'}`;
+    const template = 'oops... "{{property}}" is missing;error: {{error}}';
     expect(
-      val({
+      message(template, {
         property: 'description',
         error: 'expected property to be truthy',
       }),
-    ).toEqual('oops... description missing;error: expected property to be truthy');
+    ).toEqual('oops... "description" is missing;error: expected property to be truthy');
+  });
+
+  test('handles siblings', () => {
+    const template = '{{error}}{{error}}{{property}}{{bar}}{{error}}{{error}}';
+    expect(
+      message(template, {
+        property: 'baz',
+        error: 'foo',
+      }),
+    ).toEqual('foofoobazfoofoo');
+  });
+
+  test('strips missing keys', () => {
+    const template = '{{foo}}missing {{bar}}:(';
+    expect(
+      message(template, {
+        property: 'description',
+        error: 'expected property to be truthy',
+      }),
+    ).toEqual('missing :(');
   });
 });
