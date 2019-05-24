69a5341b3a5db5dc82953c9c355231be59634489
Jakub Rożek <P0lip@users.noreply.github.com>
2019-05-24 17:46:35 +0200

    fix: invalid-ref error (#228)
    * fix: handle ajv resolving issues
    
    * feat: include resolver errors

--- src/__tests__/linter.test.ts
+++ src/__tests__/linter.test.ts
@@ -12,6 +12,7 @@ const invalidSchema = fs.readFileSync(
   path.join(__dirname, './__fixtures__/petstore.invalid-schema.oas3.yaml'),
   'utf-8',
 );
+const todosInvalid = fs.readFileSync(path.join(__dirname, './__fixtures__/todos.invalid.oas2.json'), 'utf-8');
 
 const fnName = 'fake';
 const fnName2 = 'fake2';
@@ -205,26 +206,69 @@ responses:: !!foo
 
     const result = await spectral.run(invalidSchema);
 
-    expect(result).toEqual([
-      expect.objectContaining({
-        code: 'oas3-schema',
-        message: 'should NOT have additional properties: type',
-        summary: 'should NOT have additional properties: type',
-        path: ['paths', '/pets', 'get', 'responses', '200', 'headers', 'header-1'],
-      }),
-      expect.objectContaining({
-        code: 'oas3-schema',
-        message: 'should match exactly one schema in oneOf',
-        summary: 'should match exactly one schema in oneOf',
-        path: ['paths', '/pets', 'get', 'responses', '200', 'headers', 'header-1'],
-      }),
-      expect.objectContaining({
-        code: 'oas3-schema',
-        message: "should have required property '$ref'",
-        summary: "should have required property '$ref'",
-        path: ['paths', '/pets', 'get', 'responses', '200', 'headers', 'header-1'],
-      }),
-    ]);
+    expect(result).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          code: 'oas3-schema',
+          message: 'should NOT have additional properties: type',
+          summary: 'should NOT have additional properties: type',
+          path: ['paths', '/pets', 'get', 'responses', '200', 'headers', 'header-1'],
+        }),
+        expect.objectContaining({
+          code: 'oas3-schema',
+          message: 'should match exactly one schema in oneOf',
+          summary: 'should match exactly one schema in oneOf',
+          path: ['paths', '/pets', 'get', 'responses', '200', 'headers', 'header-1'],
+        }),
+        expect.objectContaining({
+          code: 'oas3-schema',
+          message: "should have required property '$ref'",
+          summary: "should have required property '$ref'",
+          path: ['paths', '/pets', 'get', 'responses', '200', 'headers', 'header-1'],
+        }),
+      ]),
+    );
+  });
+
+  test('should report invalid schema $refs', async () => {
+    spectral.addRules(oas3Ruleset.rules as RuleCollection);
+    spectral.addFunctions(oas3Functions());
+
+    const result = await spectral.run(todosInvalid);
+
+    expect(result).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          code: 'valid-example',
+          message: '"schema" property can\'t resolve reference #/parameters/missing from id #',
+          path: ['paths', '/todos/{todoId}', 'put', 'parameters', 1, 'schema'],
+        }),
+      ]),
+    );
+  });
+
+  test('should report invalid $refs', async () => {
+    spectral.addRules(oas3Ruleset.rules as RuleCollection);
+    spectral.addFunctions(oas3Functions());
+
+    const result = await spectral.run(invalidSchema);
+
+    expect(result).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          code: 'invalid-ref',
+          message: "No reader defined for scheme 'file' in ref file://models/pet.yaml",
+          path: ['paths', '/pets', 'get', 'responses', '200', 'content', 'application/json', 'schema', '$ref'],
+          severity: DiagnosticSeverity.Error,
+        }),
+        expect.objectContaining({
+          code: 'invalid-ref',
+          message: "No reader defined for scheme 'file' in ref file://../common/models/error.yaml",
+          path: ['paths', '/pets', 'get', 'responses', 'default', 'content', 'application/json', 'schema', '$ref'],
+          severity: DiagnosticSeverity.Error,
+        }),
+      ]),
+    );
   });
 
   describe('functional tests for the given property', () => {
