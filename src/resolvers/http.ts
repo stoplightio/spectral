26dcb3f7d713453e1eedf0d1530bb757c53af6cd
Jakub Rożek <jakub@stoplight.io>
2019-06-04 16:37:49 +0800

    fix: missing function call

--- src/resolvers/http.ts
+++ src/resolvers/http.ts
@@ -2,8 +2,8 @@ import { Resolver } from '@stoplight/json-ref-resolver';
 import unfetch from 'unfetch';
 
 export const httpReader = {
-  async read(ref: any) {
-    return unfetch(String(ref)).then(r => r.text);
+  async read(ref: unknown) {
+    return (await unfetch(String(ref))).text();
   },
 };
 
