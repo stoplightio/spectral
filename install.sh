c3df4c855446df84a0f4f172bb7df1f309e79e54
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-06-27 12:06:30 +0200

    chore: add install sh script

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+install () {
+
+set -eu
+
+UNAME=$(uname)
+if [ "$UNAME" != "Linux" ] && [ "$UNAME" != "Darwin" ] ; then
+  echo "Sorry, OS/Architecture not supported: ${UNAME}/${ARCH}. Download binary from https://github.com/stoplightio/spectral/releases"
+  exit 1
+fi
+
+if [ "$UNAME" = "Darwin" ] ; then
+  PLATFORM="macos"
+elif [ "$UNAME" = "Linux" ] ; then
+  PLATFORM="linux"
+fi
+
+URL="https://github.com/stoplightio/spectral/releases/latest/download/spectral-cli-$PLATFORM"
+SRC=$(pwd)/spectral-$PLATFORM
+DEST=/usr/local/bin/spectral
+
+STATUS=$(curl -sL -w %{http_code} -o $SRC $URL)
+if [ $STATUS -ge 200 ] & [ $STATUS -le 308 ]; then
+  mv $SRC $DEST
+  chmod +x $DEST
+  echo "Spectral installation was successful"
+else
+  rm $SRC
+  echo "Error requesting. Download binary from ${URL}"
+  exit 1
+fi
+}
+
+install
