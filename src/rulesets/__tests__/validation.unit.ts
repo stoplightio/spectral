d958c657c4469a3dd5f648df17969c80d01cfb82
Jakub Rożek <jakub@stoplight.io>
2019-07-01 19:11:49 +0200

    feat: refine ruleset validator

--- src/rulesets/__tests__/validation.unit.ts
+++ src/rulesets/__tests__/validation.unit.ts
@@ -1,13 +1,13 @@
-import { validateRuleset } from '../validation';
+import { assertValidRuleset } from '../validation';
 const invalidRuleset = require('./__fixtures__/ruleset-invalid.json');
 const validRuleset = require('./__fixtures__/valid-ruleset.json');
 
 describe('Ruleset Validation', () => {
-  it('given invalid ruleset should emit errors', () => {
-    expect(JSON.stringify(validateRuleset(invalidRuleset), null, 2)).toMatchSnapshot();
+  it('given invalid ruleset should throw', () => {
+    expect(assertValidRuleset.bind(null, invalidRuleset)).toThrow('Provided ruleset is not valid');
   });
 
   it('given valid ruleset should emit no errors', () => {
-    expect(validateRuleset(validRuleset)).toEqual([]);
+    expect(assertValidRuleset.bind(null, validRuleset)).not.toThrow();
   });
 });
--- src/rulesets/validation.ts
+++ src/rulesets/validation.ts
@@ -1,14 +1,16 @@
+import { IRulesetFile } from '../types/ruleset';
+
 const AJV = require('ajv');
-import { ErrorObject } from 'ajv';
 import * as ruleSchema from '../meta/rule.schema.json';
 import * as rulesetSchema from '../meta/ruleset.schema.json';
 
 const ajv = new AJV({ allErrors: true, jsonPointers: true });
 const validate = ajv.addSchema(ruleSchema).compile(rulesetSchema);
 
-export function validateRuleset(ruleset: object): ErrorObject[] {
-  if (validate(ruleset)) {
-    return [];
+export function assertValidRuleset(ruleset: unknown): IRulesetFile {
+  if (ruleset === null || typeof ruleset !== 'object' || !('rules' in ruleset!) || !validate(ruleset)) {
+    throw new Error('Provided ruleset is not valid');
   }
-  return validate.errors || [];
+
+  return ruleset as IRulesetFile;
 }
