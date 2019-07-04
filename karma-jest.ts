86a24d3a2caa561caa49d4aa97df62d2b05905d1
Jakub Rożek <jakub@stoplight.io>
2019-07-04 20:20:55 +0200

    build: tweak karma-jest.ts

--- karma-jest.ts
+++ karma-jest.ts
@@ -1,14 +1,19 @@
-// @ts-ignore
-window.jest = require('jest-mock');
-// @ts-ignore
-window.expect = require('expect');
-window.test = it;
+(global as any).jest = require('jest-mock');
+(global as any).expect = require('expect');
+(global as any).test = it;
+
+expect.extend({
+  toMatchSnapshot: () => ({ pass: true, message: '' }),
+  toMatchInlineSnapshot: () => ({ pass: true, message: '' }),
+});
 
 // @ts-ignore
-window.expect.extend({
-  toMatchSnapshot: () => ({ pass: true }),
-  toMatchInlineSnapshot: () => ({ pass: true }),
-});
+test.each = input => (name: string, fn: Function) => {
+  // very simple stub-like implementation needed by src/rulesets/oas3/__tests__/valid-example.ts
+  for (const value of input) {
+    fn(name.replace('%s', value[0]), ...value);
+  }
+};
 
 beforeEach(() => {
   jest.restoreAllMocks();
