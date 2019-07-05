00ba6f88b882a79a3a6e47474f0fcb4de3bfd1d2
Jakub Rożek <jakub@stoplight.io>
2019-07-05 22:25:20 +0200

    fix: rule with human-readable severity should be considered as valid

--- src/rulesets/__tests__/validation.test.ts
+++ src/rulesets/__tests__/validation.test.ts
@@ -2,6 +2,10 @@ import { assertValidRuleset } from '../validation';
 const invalidRuleset = require('./__fixtures__/invalid-ruleset.json');
 const validRuleset = require('./__fixtures__/valid-flat-ruleset.json');
 
+// @oclif/test packages requires @types/mocha, therefore we have 2 packages coming up with similar typings
+// TS is confused and prefers the mocha ones, so we need to instrument it to pick up the Jest ones
+declare var it: jest.It;
+
 describe('Ruleset Validation', () => {
   it('given invalid ruleset should throw', () => {
     expect(assertValidRuleset.bind(null, invalidRuleset)).toThrow();
@@ -11,11 +15,25 @@ describe('Ruleset Validation', () => {
     expect(assertValidRuleset.bind(null, validRuleset)).not.toThrow();
   });
 
-  it('recognizes severity flags', () => {
+  it.each(['error', 'warn', 'info', 'hint', 'off'])('recognizes human-readable %s severity', severity => {
     expect(
       assertValidRuleset.bind(null, {
         rules: {
-          rule: 'off',
+          rule: severity,
+        },
+      }),
+    ).not.toThrow();
+
+    expect(
+      assertValidRuleset.bind(null, {
+        rules: {
+          rule: {
+            given: '$.info',
+            then: {
+              function: 'truthy',
+            },
+            severity,
+          },
         },
       }),
     ).not.toThrow();
