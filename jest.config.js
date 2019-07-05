fc03905198db2e4ac0e15f0bbc61b7dc844abd39
Jakub Rożek <jakub@stoplight.io>
2019-07-05 22:52:53 +0200

    chore: drop electron runner

--- jest.config.js
+++ jest.config.js
@@ -3,29 +3,4 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   testEnvironment: 'node',
   testMatch: ['<rootDir>/src/**/__tests__/*.(ts)'],
-  displayName: {
-    color: 'blue',
-    name: 'Spectral Core',
-  },
-  projects: [
-    '<rootDir>',
-    {
-      preset: '@stoplight/scripts',
-      displayName: {
-        color: 'red',
-        name: 'Spectral Electron',
-      },
-      testPathIgnorePatterns: [ '/node_modules/', '<rootDir>/src/cli/'],
-      runner: '@jest-runner/electron',
-      testEnvironment: '@jest-runner/electron/environment',
-    },
-    {
-      preset: '@stoplight/scripts',
-      displayName: {
-        name: 'Spectral CLI',
-        color: 'grey',
-      },
-      testMatch: ["<rootDir>/src/cli/**/__tests__/**/*.[jt]s?(x)"]
-    }
-  ],
 };
