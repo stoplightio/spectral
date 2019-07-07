93e46f1c4776f443d953a7193a2a9b7fb7f663ba
Jakub Rożek <jakub@stoplight.io>
2019-07-07 21:52:51 +0200

    test(finder): unpkg

--- src/rulesets/__tests__/finder.karma.test.ts
+++ src/rulesets/__tests__/finder.karma.test.ts
@@ -6,4 +6,12 @@ describe('Rulesets finder', () => {
       'https://unpkg.com/@stoplight/spectral/rulesets/oas2/index.json',
     );
   });
+
+  for (const shorthand of ['oas', 'oas2', 'oas3']) {
+    it(`should support spectral built-in ${shorthand} ruleset shorthand`, () => {
+      return expect(findRuleset('', `spectral:${shorthand}`)).resolves.toEqual(
+        `https://unpkg.com/@stoplight/spectral/rulesets/${shorthand}/index.json`,
+      );
+    });
+  }
 });
