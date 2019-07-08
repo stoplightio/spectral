45fd65f59b4b070f3d8e18392fa6e9484d2cc1f8
Jakub Rożek <jakub@stoplight.io>
2019-07-08 20:41:35 +0200

    chore: remove dead code

--- src/rulesets/index.ts
+++ src/rulesets/index.ts
@@ -1,14 +1,3 @@
-import { oas2Functions } from './oas2';
-import * as oas2Ruleset from './oas2/index.json';
-import { oas3Functions } from './oas3';
-import * as oas3Ruleset from './oas3/index.json';
-
-const merge = require('lodash/merge');
-
-export const defaultRules = () => {
-  return merge(oas2Ruleset.rules, oas3Ruleset.rules);
-};
-
-export const defaultFunctions = () => {
-  return merge(oas2Functions(), oas3Functions());
-};
+export { mergeRulesets } from './merger';
+export { readRulesFromRulesets } from './reader';
+export { assertValidRuleset } from './validation';
