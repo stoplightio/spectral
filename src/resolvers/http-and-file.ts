6bfa5ad8b214719905437153c1ab881c3c9184ae
Jakub Rożek <jakub@stoplight.io>
2019-08-30 20:15:54 +0200

    feat(resolver): remove redundant parseResolveResult

--- src/resolvers/http-and-file.ts
+++ src/resolvers/http-and-file.ts
@@ -1,6 +1,4 @@
 import { Resolver } from '@stoplight/json-ref-resolver';
-import { extname } from '@stoplight/path';
-import { parse } from '@stoplight/yaml';
 import * as fs from 'fs';
 
 import { httpReader } from './http';
@@ -15,23 +13,14 @@ export const httpAndFileResolver = new Resolver({
         return new Promise((resolve, reject) => {
           const path = ref.path();
           fs.readFile(path, 'utf8', (err, data) => {
-            if (err) reject(err);
-            resolve(data);
+            if (err) {
+              reject(err);
+            } else {
+              resolve(data);
+            }
           });
         });
       },
     },
-  },
-
-  parseResolveResult: async opts => {
-    const ext = extname(opts.targetAuthority.toString());
-
-    if (ext === '.yml' || ext === '.yaml') {
-      opts.result = parse(opts.result);
-    } else if (ext === '.json') {
-      opts.result = JSON.parse(opts.result);
-    }
-
-    return opts;
   },
 });
