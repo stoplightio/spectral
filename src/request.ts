893a433f7bbef661dae10f4c1ad31b8b5ed5e2ae
lag-of-death <mateuszwit21@gmail.com>
2019-09-16 12:06:16 +0200

    fix: do not throw when accessing process.env.PROXY
    Co-Authored-By: Jakub Rożek <P0lip@users.noreply.github.com>

--- src/request.ts
+++ src/request.ts
@@ -3,7 +3,7 @@ import * as fetch from 'node-fetch';
 import * as ProxyAgent from 'proxy-agent';
 
 export default (uri: string, opts: any = {}) => {
-  const options = process.env.PROXY ? { ...opts, agent: new ProxyAgent(process.env.PROXY) } : opts;
+  const options = typeof process === 'object' && process.env.PROXY ? { ...opts, agent: new ProxyAgent(process.env.PROXY) } : opts;
 
   return fetch(uri, options);
 };
