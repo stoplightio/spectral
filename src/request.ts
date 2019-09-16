e8e5da26ebb90a605a89103e8266ae241e0758e1
Mateusz Wit <mateuszwit21@gmail.com>
2019-09-16 12:19:00 +0200

    refactor: add types

--- src/request.ts
+++ src/request.ts
@@ -1,9 +1,11 @@
-// @ts-ignore
-import * as fetch from 'node-fetch';
+import { RequestInit } from 'node-fetch';
 import * as ProxyAgent from 'proxy-agent';
 
-export default (uri: string, opts: any = {}) => {
-  const options = typeof process === 'object' && process.env.PROXY ? { ...opts, agent: new ProxyAgent(process.env.PROXY) } : opts;
+const fetch = require('node-fetch');
+
+export default (uri: string, opts: RequestInit = {}) => {
+  const options =
+    typeof process === 'object' && process.env.PROXY ? { ...opts, agent: new ProxyAgent(process.env.PROXY) } : opts;
 
   return fetch(uri, options);
 };
