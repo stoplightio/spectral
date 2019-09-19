a2634bb54d1bc370092b38a708ff084be814c80d
Jakub Rożek <P0lip@users.noreply.github.com>
2019-09-19 12:11:57 +0200

    fix(stylish): support hint and print proper severity (#569)

--- src/formatters/__tests__/stylish.test.ts
+++ src/formatters/__tests__/stylish.test.ts
@@ -1,12 +1,33 @@
+import chalk from 'chalk';
 import { stylish } from '../stylish';
 
-const results = require('./__fixtures__/oas3-schema-errors.json');
+const oas3SchemaErrors = require('./__fixtures__/oas3-schema-errors.json');
+const mixedErrors = require('./__fixtures__/mixed-errors.json');
 
 describe('Stylish formatter', () => {
   test('should prefer message for oas-schema errors', () => {
-    const result = stylish(results);
+    const result = stylish(oas3SchemaErrors);
     expect(result).toContain('oas3-schema  should NOT have additional properties: type\n');
     expect(result).toContain('oas3-schema  should match exactly one schema in oneOf');
     expect(result).toContain("oas3-schema  should have required property '$ref'\n");
+  });
+
+  test('should display proper severity level', () => {
+    const result = stylish(mixedErrors);
+    expect(result).toContain(`
+  3:10         ${chalk.white('hint')}  info-contact            Info object should contain \`contact\` object.
+  3:10      ${chalk.yellow(
+    'warning',
+  )}  info-description        OpenAPI object info \`description\` must be present and non-empty string.
+  5:14        ${chalk.red('error')}  info-matches-stoplight  Info must contain Stoplight
+ 17:13  ${chalk.blue(
+   'information',
+ )}  operation-description   Operation \`description\` must be present and non-empty string.
+ 64:14  ${chalk.blue(
+   'information',
+ )}  operation-description   Operation \`description\` must be present and non-empty string.
+ 86:13  ${chalk.blue(
+   'information',
+ )}  operation-description   Operation \`description\` must be present and non-empty string.`);
   });
 });
