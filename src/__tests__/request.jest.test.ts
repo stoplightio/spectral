1cce8385c94b62232dba7afb1772707fdfdc7101
Jakub Rożek <jakub@stoplight.io>
2019-09-25 00:17:23 +0200

    feat: do not load http-agent at all if not in cli

--- src/__tests__/request.jest.test.ts
+++ src/__tests__/request.jest.test.ts
@@ -1,78 +1,81 @@
 import * as http from 'http';
 import * as url from 'url';
+import { DEFAULT_REQUEST_OPTIONS } from '../request';
 import { httpAndFileResolver } from '../resolvers/http-and-file';
 import { Spectral } from '../spectral';
+const ProxyAgent = require('proxy-agent');
 
 const PORT = 4001;
 
 describe('request', () => {
-  const oldProxyEnv = process.env.PROXY;
-  let server: http.Server;
+  describe('when agent is set', () => {
+    let server: http.Server;
 
-  beforeAll(() => {
-    // nock cannot mock proxied requests
-    server = http
-      .createServer((req, res) => {
-        const { pathname } = url.parse(String(req.url));
-        if (pathname === '/custom-ruleset') {
-          res.writeHead(403);
-        } else if (pathname === '/ok.json') {
-          res.writeHead(200, { 'Content-Type': 'application/json' });
-          res.write(
-            JSON.stringify({
-              info: {
-                title: '',
-                description: 'Foo',
-              },
-            }),
-          );
-        } else {
-          res.writeHead(404);
-        }
+    beforeAll(() => {
+      // nock cannot mock proxied requests
+      server = http
+        .createServer((req, res) => {
+          const { pathname } = url.parse(String(req.url));
+          if (pathname === '/custom-ruleset') {
+            res.writeHead(403);
+          } else if (pathname === '/ok.json') {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(
+              JSON.stringify({
+                info: {
+                  title: '',
+                  description: 'Foo',
+                },
+              }),
+            );
+          } else {
+            res.writeHead(404);
+          }
 
-        res.end();
-      })
-      .listen(PORT, '0.0.0.0');
-  });
-
-  afterAll(() => {
-    server.close();
-  });
-
-  beforeEach(() => {
-    process.env.PROXY = `http://localhost:${PORT}/`;
-  });
-
-  afterEach(() => {
-    process.env.PROXY = oldProxyEnv;
-  });
-
-  describe('loading a ruleset', () => {
-    it('proxies the request', () => {
-      const s = new Spectral();
-
-      return expect(s.loadRuleset('http://localhost:4000/custom-ruleset')).rejects.toHaveProperty(
-        'message',
-        'Could not parse http://localhost:4000/custom-ruleset: Forbidden',
-      );
+          res.end();
+        })
+        .listen(PORT, '0.0.0.0');
     });
-  });
 
-  describe('loading a $ref', () => {
-    it('proxies the request', () => {
-      const doc = {
-        info: {
-          $ref: 'http://localhost:8089/ok.json#/info',
-        },
-      };
+    afterAll(() => {
+      server.close();
+    });
 
-      const s = new Spectral({ resolver: httpAndFileResolver });
+    beforeEach(() => {
+      DEFAULT_REQUEST_OPTIONS.agent = new ProxyAgent(`http://localhost:${PORT}`);
+    });
 
-      return expect(s.runWithResolved(doc)).resolves.toHaveProperty('resolved', {
-        info: {
-          title: '',
-          description: 'Foo',
-        },
+    afterEach(() => {
+      delete DEFAULT_REQUEST_OPTIONS.agent;
+    });
+
+    describe('loading a ruleset', () => {
+      it('proxies the request', () => {
+        const s = new Spectral();
+
+        return expect(s.loadRuleset('http://localhost:4000/custom-ruleset')).rejects.toHaveProperty(
+          'message',
+          'Could not parse http://localhost:4000/custom-ruleset: Forbidden',
+        );
+      });
+    });
+
+    describe('loading a $ref', () => {
+      it('proxies the request', () => {
+        const doc = {
+          info: {
+            $ref: 'http://localhost:8089/ok.json#/info',
+          },
+        };
+
+        const s = new Spectral({ resolver: httpAndFileResolver });
+
+        return expect(s.runWithResolved(doc)).resolves.toHaveProperty('resolved', {
+          info: {
+            title: '',
+            description: 'Foo',
+          },
+        });
       });
     });
   });
--- src/request.ts
+++ src/request.ts
@@ -1,16 +1,7 @@
 import fetch, { RequestInit } from 'node-fetch';
 
+export const DEFAULT_REQUEST_OPTIONS: RequestInit = {};
+
 export default async (uri: string, opts: RequestInit = {}) => {
-  let options = opts;
-
-  if (typeof process === 'object' && process.env.PROXY) {
-    try {
-      const ProxyAgent = await import('proxy-agent');
-      options = Object.assign(opts, { agent: new ProxyAgent(process.env.PROXY) });
-    } catch (ex) {
-      console.error(ex.message);
-    }
-  }
-
-  return fetch(uri, options);
+  return fetch(uri, { ...opts, ...DEFAULT_REQUEST_OPTIONS });
 };
