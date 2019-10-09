52bfcc08d34c6bbda1702e8731eac3ce86f0b2b2
Karol Maciaszek <karol@maciaszek.pl>
2019-10-09 16:46:20 +0200

    refactor: using file/http resolvers from separate package

--- src/resolvers/http-and-file.ts
+++ src/resolvers/http-and-file.ts
@@ -1,26 +1,11 @@
 import { Resolver } from '@stoplight/json-ref-resolver';
-import * as fs from 'fs';
-
-import { httpReader } from './http';
+import { resolveFile, resolveHttp } from '@stoplight/ref-resolvers';
 
 // resolves files, http and https $refs, and internal $refs
 export const httpAndFileResolver = new Resolver({
   resolvers: {
-    https: httpReader,
-    http: httpReader,
-    file: {
-      resolve(ref: any) {
-        return new Promise((resolve, reject) => {
-          const path = ref.path();
-          fs.readFile(path, 'utf8', (err, data) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(data);
-            }
-          });
-        });
-      },
-    },
+    https: { resolve: resolveHttp },
+    http: { resolve: resolveHttp },
+    file: { resolve: resolveFile },
   },
 });
