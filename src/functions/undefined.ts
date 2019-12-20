c4935df03b7d62eab745f8e075bf8d4533329684
Jakub Rożek <jakub@stoplight.io>
2019-12-20 12:27:12 +0100

    fix: message in undefined fn

--- src/functions/undefined.ts
+++ src/functions/undefined.ts
@@ -4,7 +4,7 @@ export const undefined: IFunction = (targetVal): void | IFunctionResult[] => {
   if (typeof targetVal !== 'undefined') {
     return [
       {
-        message: '{{missingPropertyPath}} property should not be undefined',
+        message: '{{missingPropertyPath}} property should be undefined',
       },
     ];
   }
