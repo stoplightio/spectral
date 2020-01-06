282cef3b19319a123fa5a3b05fe49a0aba2c8a29
Johan Lövdahl <johan.lovdahl@gmail.com>
2020-01-06 14:57:20 +0100

    fix: ruleset path for Example Object validation (#882)
    The 'example' field has 'Any' values in all contexts
    it can be used so the rule should not be triggered for that field.
    
    The 'examples' field always has values of Example Object type in
    all contexts, so this is what the rule should trigger on.
    
    Changed tests to validate `examples` instead of `example` and added
    additional tests with multiple examples below the `examples` field.
    
    Co-authored-by: Phil Sturgeon <me@philsturgeon.uk>

--- src/rulesets/oas/__tests__/example-value-or-externalValue.ts
+++ src/rulesets/oas/__tests__/example-value-or-externalValue.ts
@@ -12,30 +12,65 @@ describe('example-value-or-externalValue', () => {
   });
 
   test('validate if just externalValue', async () => {
-    const results = await s.run({ example: { externalValue: 'value' } });
+    const results = await s.run({ examples: { first: { externalValue: 'value' } } });
     expect(results.length).toEqual(0);
   });
 
   test('validate if just value', async () => {
-    const results = await s.run({ example: { value: 'value' } });
+    const results = await s.run({ examples: { first: { value: 'value' } } });
+    expect(results.length).toEqual(0);
+  });
+
+  test('multiple examples - validate all value or externalValue', async () => {
+    const results = await s.run({
+      examples: {
+        first: { value: 'value1' },
+        second: { externalValue: 'external-value2' },
+        third: { value: 'value3' },
+      },
+    });
     expect(results.length).toEqual(0);
   });
 
   test('return warnings if missing externalValue and value', async () => {
-    const results = await s.run({ example: {} });
+    const results = await s.run({ examples: { first: {} } });
     expect(results).toEqual([
       {
         code: 'example-value-or-externalValue',
         message: 'Example should have either a `value` or `externalValue` field.',
-        path: ['example'],
+        path: ['examples', 'first'],
         range: {
           end: {
             character: 15,
-            line: 1,
+            line: 2,
           },
           start: {
             character: 12,
-            line: 1,
+            line: 2,
+          },
+        },
+        severity: DiagnosticSeverity.Warning,
+      },
+    ]);
+  });
+
+  test('multiple examples - return warnings if missing externalValue and value in one', async () => {
+    const results = await s.run({
+      examples: { first: { value: 'value1' }, second: { externalValue: 'external-value2' }, third: {} },
+    });
+    expect(results).toEqual([
+      {
+        code: 'example-value-or-externalValue',
+        message: 'Example should have either a `value` or `externalValue` field.',
+        path: ['examples', 'third'],
+        range: {
+          end: {
+            character: 15,
+            line: 8,
+          },
+          start: {
+            character: 12,
+            line: 8,
           },
         },
         severity: DiagnosticSeverity.Warning,
@@ -44,20 +79,48 @@ describe('example-value-or-externalValue', () => {
   });
 
   test('return warnings if both externalValue and value', async () => {
-    const results = await s.run({ example: { externalValue: 'externalValue', value: 'value' } });
+    const results = await s.run({ examples: { first: { externalValue: 'externalValue', value: 'value' } } });
     expect(results).toEqual([
       {
         code: 'example-value-or-externalValue',
         message: 'Example should have either a `value` or `externalValue` field.',
-        path: ['example'],
+        path: ['examples', 'first'],
         range: {
           end: {
-            character: 20,
-            line: 3,
+            character: 22,
+            line: 4,
           },
           start: {
             character: 12,
-            line: 1,
+            line: 2,
+          },
+        },
+        severity: DiagnosticSeverity.Warning,
+      },
+    ]);
+  });
+
+  test('multiple examples - return warnings if both externalValue and value in one', async () => {
+    const results = await s.run({
+      examples: {
+        first: { value: 'value1' },
+        second: { externalValue: 'external-value2', value: 'value2' },
+        third: { externalValue: 'external-value3' },
+      },
+    });
+    expect(results).toEqual([
+      {
+        code: 'example-value-or-externalValue',
+        message: 'Example should have either a `value` or `externalValue` field.',
+        path: ['examples', 'second'],
+        range: {
+          end: {
+            character: 23,
+            line: 7,
+          },
+          start: {
+            character: 13,
+            line: 5,
           },
         },
         severity: DiagnosticSeverity.Warning,
