e06c95414917bfe75ddec565295a280c28f5aa8b
Jakub Rożek <jakub@stoplight.io>
2020-04-12 19:07:55 +0200

    chore: document oasDocumentSchema fn

--- src/rulesets/oas/functions/oasDocumentSchema.ts
+++ src/rulesets/oas/functions/oasDocumentSchema.ts
@@ -4,12 +4,15 @@ import { IFunction, IFunctionContext, IFunctionResult } from '../../../types';
 
 function shouldIgnoreError(error: AJV.ErrorObject) {
   return (
+    // oneOf is a fairly error as we have 2 options to choose from for most of the time.
     error.keyword === 'oneOf' ||
+    // the required $ref is entirely useless, since oas-schema rules operate on resolved content, so there won't be any $refs in the document
     (error.keyword === 'required' && (error.params as AJV.RequiredParams).missingProperty === '$ref')
   );
 }
 
-// this is supposed to cover edge cases, when it's impossible to detect the most appropriate error, i.e. oneOf consisting of more than 3 members, etc.
+// this is supposed to cover edge cases we need to cover manually, when it's impossible to detect the most appropriate error, i.e. oneOf consisting of more than 3 members, etc.
+// note,  more errors can be included if certain messages reported by AJV are not quite meaningful
 const ERROR_MAP = [
   {
     path: /^components\/securitySchemes\/[^/]+$/,
@@ -21,7 +24,12 @@ const ERROR_MAP = [
   },
 ];
 
-// /shrug
+// The function removes irrelevant (aka misleading, confusing, useless, whatever you call it) errors.
+// There are a few exceptions, i.e. security components I covered manually,
+// yet apart from them we usually deal with a relatively simple scenario that can be literally expressed as: "either proper value of $ref property".
+// The $ref part is never going to be interesting for us, because both oas-schema rules operate on resolved content, so we won't have any $refs left.
+// As you can see, what we deal here wit is actually not really oneOf anymore - it's always the first member of oneOf we match against.
+// That being said, we always strip both oneOf and $ref, since we are always interested in the first error.
 export function prepareResults(errors: AJV.ErrorObject[]) {
   for (let i = 0; i < errors.length; i++) {
     const error = errors[i];
