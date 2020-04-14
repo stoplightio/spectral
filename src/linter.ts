69429c6e0de079bd2dff17072abdaccf1f41ad28
Jakub Rożek <jakub@stoplight.io>
2020-04-14 15:59:10 +0200

    revert: pass default empty obj options

--- src/linter.ts
+++ src/linter.ts
@@ -64,7 +64,7 @@ export const lintNode = async (
 
       const targetResults = await func(
         target.value,
-        then.functionOptions,
+        then.functionOptions || {},
         {
           given: givenPath,
           target: targetPath,
