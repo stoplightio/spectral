a7af63ca3c3b23c5975b16be22552db3b8d10eef
Jakub Rożek <jakub@stoplight.io>
2020-05-26 14:24:43 +0200

    feat: custom functions lifecycle events (#1169)
    * feat: beforeTeardown & afterTeardown
    
    * feat: add setup
    
    * chore: wrap it up
    
    * docs: cover lifecycle events
    
    * docs: redundant new-line
    
    * docs: extra warning
    
    * docs: scrap the content

--- src/runner/types.ts
+++ src/runner/types.ts
@@ -3,13 +3,13 @@ import { FunctionCollection, IRuleResult, RunRuleCollection } from '../types';
 import { RulesetExceptionCollection } from '../types/ruleset';
 
 export interface IRunnerPublicContext {
-  documentInventory: DocumentInventory;
   rules: RunRuleCollection;
   functions: FunctionCollection;
   exceptions: RulesetExceptionCollection;
 }
 
 export interface IRunnerInternalContext extends IRunnerPublicContext {
+  documentInventory: DocumentInventory;
   results: IRuleResult[];
   promises: Array<Promise<void>>;
 }
