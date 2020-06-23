5da8d335f910c30205bbb37dcefb950bb587b440
Jakub Rożek <jakub@stoplight.io>
2020-06-23 17:59:28 +0200

    fix(oas): operation-paramaters outputs better messages (#1240)
    * fix(oas): operation-paramaters outputs better messages
    
    * fix: typo
    
    * feat: more errors

--- src/rulesets/oas/functions/oasOpParams.ts
+++ src/rulesets/oas/functions/oasOpParams.ts
@@ -1,137 +1,76 @@
 import type { IFunction, IFunctionResult } from '../../../types';
+import type { Dictionary } from '@stoplight/types';
 
-export const oasOpParams: IFunction = (targetVal, _options, _paths, vals) => {
-  const results: IFunctionResult[] = [];
+function computeFingerprint(param: Dictionary<unknown>): string {
+  return `${param.in}-${param.name}`;
+}
 
-  const { original: object } = vals;
-
+export const oasOpParams: IFunction = (params, _opts, { given }) => {
   /**
-   * This rule verifies:
+   * This function verifies:
    *
    * 1. Operations must have unique `name` + `in` parameters.
    * 2. Operation cannot have both `in:body` and `in:formData` parameters
    * 3. Operation must have only one `in:body` parameter.
    */
 
-  if (!object.paths) {
-    return [];
+  if (!Array.isArray(params)) return;
+
+  if (params.length < 2) return;
+
+  const results: IFunctionResult[] = [];
+
+  const count: Dictionary<number[]> = {
+    body: [],
+    formData: [],
+  };
+  const list: string[] = [];
+  const duplicates: number[] = [];
+
+  let index = -1;
+
+  for (const param of params) {
+    index++;
+
+    if (param === null || typeof param !== 'object') continue;
+
+    // skip params that are refs
+    if ('$ref' in param) continue;
+
+    // Operations must have unique `name` + `in` parameters.
+    const fingerprint = computeFingerprint(param);
+    if (list.includes(fingerprint)) {
+      duplicates.push(index);
+    } else {
+      list.push(fingerprint);
+    }
+
+    if (param.in in count) {
+      count[param.in].push(index);
+    }
   }
 
-  for (const path in object.paths) {
-    if (!object.paths[path]) continue;
+  if (duplicates.length > 0) {
+    for (const i of duplicates) {
+      results.push({
+        message: 'A parameter in this operation already exposes the same combination of `name` and `in` values.',
+        path: [...given, i],
+      });
+    }
+  }
 
-    for (const operation in object.paths[path]) {
-      if (!object.paths[path][operation] || operation === 'parameters') continue;
+  if (count.body.length > 0 && count.formData.length > 0) {
+    results.push({
+      message: 'Operation cannot have both `in:body` and `in:formData` parameters.',
+    });
+  }
 
-      // get operation parameters
-      const params: any[] = object.paths[path][operation].parameters || [];
-
-      const nonUnique = {};
-      const inBoth = {};
-      const inBody = {};
-
-      if (params.length > 1) {
-        for (const paramIndex of params.keys()) {
-          // skip params that are refs
-          if (!params[paramIndex] || params[paramIndex].$ref) continue;
-
-          for (const compareIndex of params.keys()) {
-            // don't run rules on itself or pairs that are already tested
-            if (paramIndex <= compareIndex || params[compareIndex].$ref) continue;
-
-            // Operations must have unique `name` + `in` parameters.
-            if (
-              params[paramIndex].in === params[compareIndex].in &&
-              params[paramIndex].name === params[compareIndex].name
-            ) {
-              if (!nonUnique[`${params[paramIndex].in}-${params[paramIndex].name}`]) {
-                nonUnique[`${params[paramIndex].in}-${params[paramIndex].name}`] = {};
-              }
-
-              nonUnique[`${params[paramIndex].in}-${params[paramIndex].name}`][paramIndex] = params[paramIndex];
-
-              nonUnique[`${params[paramIndex].in}-${params[paramIndex].name}`][compareIndex] = params[compareIndex];
-            }
-
-            // Operation cannot have both `in:body` and `in:formData` parameters
-
-            if (
-              // skip param that already tested true
-              !inBoth[paramIndex] &&
-              ((params[paramIndex].in === 'body' && params[compareIndex].in === 'formData') ||
-                (params[paramIndex].in === 'formData' && params[compareIndex].in === 'body'))
-            ) {
-              inBoth[paramIndex] = params[paramIndex];
-              inBoth[compareIndex] = params[compareIndex];
-            }
-
-            // Operation must have only one `in:body` parameter.
-            if (
-              // skip param that already tested true
-              !inBody[paramIndex] &&
-              params[paramIndex].in === 'body' &&
-              params[compareIndex].in === 'body'
-            ) {
-              inBody[paramIndex] = params[paramIndex];
-              inBody[compareIndex] = params[compareIndex];
-              //
-            }
-          }
-        }
-
-        if (Object.keys(nonUnique).length > 0) {
-          for (const group in nonUnique) {
-            if (!nonUnique[group]) continue;
-
-            const firstMatch = Object.keys(nonUnique[group])[0];
-            const inVal = nonUnique[group][firstMatch].in;
-            const nameVal = nonUnique[group][firstMatch].name;
-
-            let message = `Operations must have unique \`name\` + \`in\` parameters.\nRepeats of \`in:${inVal}\` + \`name:${nameVal}\`\n\nParameters found at:\n`;
-
-            for (const index in nonUnique[group]) {
-              if (!inBody[index]) {
-                message += `[ \`paths\`, \`${path}\`, \`${operation}\`, \`${index}\` ]\n`;
-              }
-            }
-
-            results.push({
-              message,
-              path: ['paths', path, operation],
-            });
-          }
-        }
-
-        if (Object.keys(inBoth).length > 1) {
-          let message = 'Operation cannot have both `in:body` and `in:formData` parameters.\n\nParameters found at:\n';
-
-          for (const index in inBoth) {
-            if (!inBody[index]) {
-              message += `[ \`paths\`, \`${path}\`, \`${operation}\`, \`${index}\` ]\n`;
-            }
-          }
-
-          results.push({
-            message,
-            path: ['paths', path, operation],
-          });
-        }
-
-        if (Object.keys(inBody).length > 1) {
-          let message = 'Operation has multiple instances of the `in:body` parameter.\n\nParameters found at:\n';
-
-          for (const index in inBody) {
-            if (!inBody[index]) {
-              message += `[ \`paths\`, \`${path}\`, \`${operation}\`, \`${index}\` ]\n`;
-            }
-          }
-
-          results.push({
-            message,
-            path: ['paths', path, operation],
-          });
-        }
-      }
+  if (count.body.length > 1) {
+    for (let i = 1; i < count.body.length; i++) {
+      results.push({
+        message: 'Operation has already at least one instance of the `in:body` parameter.',
+        path: [...given, count.body[i]],
+      });
     }
   }
 
