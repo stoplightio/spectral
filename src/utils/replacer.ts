ebe6d19ab56a2c4e22ee67085c265d9c24daa43c
Jakub Rożek <jakub@stoplight.io>
2020-09-15 16:23:13 +0200

    feat(replacer): allow code to be evaluated

--- src/utils/replacer.ts
+++ src/utils/replacer.ts
@@ -1,4 +1,5 @@
 import { Dictionary } from '@stoplight/types';
+import { eval, parse } from 'expression-eval';
 
 export type Transformer<V = unknown, VV = object> = (identifier: string, value: V, values: VV) => string;
 
@@ -7,17 +8,23 @@ export class Replacer<V extends object> {
   protected readonly transformers: Dictionary<Transformer<V[keyof V], V>>;
 
   constructor(count: number) {
-    this.regex = new RegExp(`${'{'.repeat(count)}([^}\n]+)${'}'.repeat(count)}`, 'g');
+    this.regex = new RegExp(`#?${'{'.repeat(count)}([^}\n]+)${'}'.repeat(count)}`, 'g');
 
     this.transformers = {};
   }
 
-  public addTransformer(name: string, filter: Transformer<V[keyof V], V>) {
+  public addTransformer(name: string, filter: Transformer<V[keyof V], V>): void {
     this.transformers[name] = filter;
   }
 
-  public print(input: string, values: V) {
-    return input.replace(this.regex, (substr, expr) => {
+  public print(input: string, values: V): string {
+    return input.replace(this.regex, (substr, expr, index) => {
+      const shouldEvaluate = input[index] === '#';
+
+      if (shouldEvaluate) {
+        return String(eval(parse(expr), values));
+      }
+
       const [identifier, ...transformers] = expr.split('|');
 
       if (!(identifier in values)) {
