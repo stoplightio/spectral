04f6d8f2cf8f638438933a4fb50a3b2a86d21810
Alex Burgel <aburgel@gmail.com>
2020-11-20 08:21:46 -0500

    feat(cli): add JSON path to stylish formatter (#1382)
    * feat: Add JSON path to stylish formatter
    
    * Format with printPath
    
    * Update test harness scenarios
    
    Co-authored-by: Jakub Rożek <jakub@stoplight.io>

--- src/cli/formatters/__tests__/stylish.test.ts
+++ src/cli/formatters/__tests__/stylish.test.ts
@@ -9,27 +9,31 @@ const mixedErrors = sortResults(require('./__fixtures__/mixed-errors.json'));
 describe('Stylish formatter', () => {
   test('should prefer message for oas-schema errors', () => {
     const result = stylish(oas3SchemaErrors, { failSeverity: DiagnosticSeverity.Error });
-    expect(result).toContain('oas3-schema  should NOT have additional properties: type\n');
+    expect(result).toContain('oas3-schema  should NOT have additional properties: type');
     expect(result).toContain('oas3-schema  should match exactly one schema in oneOf');
-    expect(result).toContain("oas3-schema  should have required property '$ref'\n");
+    expect(result).toContain("oas3-schema  should have required property '$ref'");
   });
 
   test('should display proper severity level', () => {
     const result = stylish(mixedErrors, { failSeverity: DiagnosticSeverity.Error });
     expect(result).toContain(`
-  3:10         ${chalk.white('hint')}  info-contact            Info object should contain \`contact\` object.
+  3:10         ${chalk.white(
+    'hint',
+  )}  info-contact            Info object should contain \`contact\` object.                             info
   3:10      ${chalk.yellow(
     'warning',
-  )}  info-description        OpenAPI object info \`description\` must be present and non-empty string.
-  5:14        ${chalk.red('error')}  info-matches-stoplight  Info must contain Stoplight
+  )}  info-description        OpenAPI object info \`description\` must be present and non-empty string.  info
+  5:14        ${chalk.red(
+    'error',
+  )}  info-matches-stoplight  Info must contain Stoplight                                              info.title
  17:13  ${chalk.blue(
    'information',
- )}  operation-description   Operation \`description\` must be present and non-empty string.
+ )}  operation-description   Operation \`description\` must be present and non-empty string.            paths./pets.get
  64:14  ${chalk.blue(
    'information',
- )}  operation-description   Operation \`description\` must be present and non-empty string.
+ )}  operation-description   Operation \`description\` must be present and non-empty string.            paths./pets.post
  86:13  ${chalk.blue(
    'information',
- )}  operation-description   Operation \`description\` must be present and non-empty string.`);
+ )}  operation-description   Operation \`description\` must be present and non-empty string.            paths./pets/{petId}.get`);
   });
 });
