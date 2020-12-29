753076f5ef4dc7ecb05d37761355f32ba1f7c46b
Jakub Rożek <jakub@stoplight.io>
2020-12-29 19:24:51 +0100

    fix(oas): oas3-examples-value-or-externalValue is too broad

--- src/rulesets/oas/__tests__/oas3-examples-value-or-externalValue.ts
+++ src/rulesets/oas/__tests__/oas3-examples-value-or-externalValue.ts
@@ -67,6 +67,26 @@ describe('oas3-examples-value-or-externalValue', () => {
     expect(results.length).toEqual(0);
   });
 
+  test('will not validate example property placed in schema under components schema', async () => {
+    const results = await s.run({
+      components: {
+        schemas: {
+          pet: {
+            properties: {
+              examples: {
+                type: 'array',
+              },
+              example: {
+                type: 'integer',
+              },
+            },
+          },
+        },
+      },
+    });
+    expect(results.length).toEqual(0);
+  });
+
   test('multiple examples - validate all value or externalValue', async () => {
     const results = await s.run({
       components: {
