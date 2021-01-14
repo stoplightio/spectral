e8c1846c5ea81402d7e79bd4f9e741b95eb46bfb
Jakub Rożek <jakub@stoplight.io>
2021-01-14 16:29:48 +0100

    fix: nested extends broken in rulesets (#1465)
    * fix(ruleset): nested extends not working correctly
    
    * Update src/rule.ts
    
    Co-authored-by: Domagoj Kriskovic <dom@stoplight.io>
    
    Co-authored-by: Domagoj Kriskovic <dom@stoplight.io>

--- src/ruleset/severity.ts
+++ src/ruleset/severity.ts
@@ -1,67 +1,7 @@
 import { DiagnosticSeverity, Dictionary } from '@stoplight/types';
 import { HumanReadableDiagnosticSeverity, SpectralDiagnosticSeverity } from '../types';
-import { FileRule, FileRuleCollection, FileRulesetSeverity, FileRuleSeverity } from '../types/ruleset';
-import { isValidRule } from './validation';
 
 export const DEFAULT_SEVERITY_LEVEL = DiagnosticSeverity.Warning;
-
-function getSeverityForRule(
-  rule: FileRule | FileRuleSeverity,
-  defaultSeverity: SpectralDiagnosticSeverity,
-): DiagnosticSeverity {
-  switch (typeof rule) {
-    case 'number':
-    case 'string':
-      return getDiagnosticSeverity(rule);
-    case 'boolean':
-      return rule ? getDiagnosticSeverity(defaultSeverity) : -1;
-    default:
-      return defaultSeverity;
-  }
-}
-
-function getSeverityForInvalidRule(
-  existingRule: FileRuleSeverity | [FileRuleSeverity] | [FileRuleSeverity, object],
-  newRule: FileRule | FileRulesetSeverity,
-) {
-  if (newRule === 'off') return -1;
-
-  if (newRule === 'recommended' || newRule === 'all') {
-    if (existingRule === false || existingRule === -1 || existingRule === 'off') {
-      return -1;
-    }
-
-    return getSeverityForRule(existingRule, DEFAULT_SEVERITY_LEVEL);
-  }
-
-  return getSeverityForRule(newRule, DEFAULT_SEVERITY_LEVEL);
-}
-
-export function getSeverityLevel(
-  rules: FileRuleCollection,
-  name: string,
-  newRule: FileRule | FileRulesetSeverity,
-): SpectralDiagnosticSeverity {
-  const existingRule = rules[name];
-
-  // this is an edge case, please refer to related tests
-  if (!isValidRule(existingRule)) {
-    return getSeverityForInvalidRule(existingRule, newRule);
-  }
-
-  const existingSeverity =
-    existingRule.severity !== undefined ? getDiagnosticSeverity(existingRule.severity) : DEFAULT_SEVERITY_LEVEL;
-
-  if (newRule === 'recommended') {
-    return existingRule.recommended !== false ? existingSeverity : -1;
-  }
-
-  if (newRule === 'all') {
-    return existingSeverity;
-  }
-
-  return getSeverityForRule(newRule, existingSeverity);
-}
 
 const SEVERITY_MAP: Dictionary<SpectralDiagnosticSeverity, HumanReadableDiagnosticSeverity> = {
   error: DiagnosticSeverity.Error,
