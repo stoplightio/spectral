95ecfa9f51dd32cac9e262bd1071aa4f6d6838e9
Jakub Rożek <jakub@rozek.tech>
2021-06-21 17:06:33 +0300

    revert: reduce diff

--- packages/cli/src/formatters/teamcity.ts
+++ packages/cli/src/formatters/teamcity.ts
@@ -1,5 +1,5 @@
 import { Dictionary, Optional } from '@stoplight/types';
-import { ISpectralDiagnostic } from '@stoplight/spectral-core';
+import { IRuleResult } from '@stoplight/spectral-core';
 import { Formatter } from './types';
 import { getSeverityName, groupBySource } from './utils';
 
@@ -19,14 +19,14 @@ function escapeString(str: Optional<string | number>): string {
     .replace(/\]/g, '|]');
 }
 
-function inspectionType(result: ISpectralDiagnostic & { source: string }): string {
+function inspectionType(result: IRuleResult & { source: string }): string {
   const code = escapeString(result.code);
   const severity = getSeverityName(result.severity);
   const message = escapeString(result.message);
   return `##teamcity[inspectionType category='openapi' id='${code}' name='${code}' description='${severity} -- ${message}']`;
 }
 
-function inspection(result: ISpectralDiagnostic & { source: string }): string {
+function inspection(result: IRuleResult & { source: string }): string {
   const code = escapeString(result.code);
   const severity = getSeverityName(result.severity);
   const message = escapeString(result.message);
@@ -34,16 +34,16 @@ function inspection(result: ISpectralDiagnostic & { source: string }): string {
   return `##teamcity[inspection typeId='${code}' file='${result.source}' line='${line}' message='${severity} -- ${message}']`;
 }
 
-function renderResults(results: ISpectralDiagnostic[]): string {
+function renderResults(results: IRuleResult[]): string {
   return results
-    .filter<ISpectralDiagnostic & { source: string }>(
-      (result): result is ISpectralDiagnostic & { source: string } => typeof result.source === 'string',
+    .filter<IRuleResult & { source: string }>(
+      (result): result is IRuleResult & { source: string } => typeof result.source === 'string',
     )
     .map(result => `${inspectionType(result)}\n${inspection(result)}`)
     .join('\n');
 }
 
-function renderGroupedResults(groupedResults: Dictionary<ISpectralDiagnostic[]>): string {
+function renderGroupedResults(groupedResults: Dictionary<IRuleResult[]>): string {
   return Object.keys(groupedResults)
     .map(source => renderResults(groupedResults[source]))
     .join('\n');
--- packages/cli/src/formatters/text.ts
+++ packages/cli/src/formatters/text.ts
@@ -1,12 +1,12 @@
 import { Dictionary } from '@stoplight/types';
-import { ISpectralDiagnostic } from '@stoplight/spectral-core';
+import { IRuleResult } from '@stoplight/spectral-core';
 import { Formatter } from './types';
 import { getSeverityName, groupBySource } from './utils';
 
-function renderResults(results: ISpectralDiagnostic[]): string {
+function renderResults(results: IRuleResult[]): string {
   return results
-    .filter<ISpectralDiagnostic & { source: string }>(
-      (result): result is ISpectralDiagnostic & { source: string } => typeof result.source === 'string',
+    .filter<IRuleResult & { source: string }>(
+      (result): result is IRuleResult & { source: string } => typeof result.source === 'string',
     )
     .map(result => {
       const line = result.range.start.line + 1;
@@ -17,7 +17,7 @@ function renderResults(results: ISpectralDiagnostic[]): string {
     .join('\n');
 }
 
-function renderGroupedResults(groupedResults: Dictionary<ISpectralDiagnostic[]>): string {
+function renderGroupedResults(groupedResults: Dictionary<IRuleResult[]>): string {
   return Object.keys(groupedResults)
     .map(source => renderResults(groupedResults[source]))
     .join('\n');
