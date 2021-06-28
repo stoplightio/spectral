3823996d93688bdd8ecd44b1a155f7f14cd5d146
Jakub Rożek <jakub@rozek.tech>
2021-06-28 13:14:12 +0200

    feat(cli): hook up ruleset-migrator

--- packages/cli/src/services/linter/utils/getRuleset.ts
+++ packages/cli/src/services/linter/utils/getRuleset.ts
@@ -2,13 +2,19 @@ import { Optional } from '@stoplight/types';
 import { Ruleset, RulesetDefinition } from '@stoplight/spectral-core';
 import * as fs from 'fs';
 import * as path from '@stoplight/path';
-import { isAbsolute } from '@stoplight/path';
 import * as process from 'process';
+import { extname } from '@stoplight/path';
+import { migrateRuleset } from '@stoplight/spectral-ruleset-migrator';
+
+const DEFAULT_RULESET_FILE = /^\.?spectral\.(ya?ml|json|m?js)$/;
+
+// eslint-disable-next-line @typescript-eslint/require-await
+const AsyncFunction = (async (): Promise<void> => void 0).constructor as FunctionConstructor;
 
 async function getDefaultRulesetFile(): Promise<Optional<string>> {
   const cwd = process.cwd();
   for (const filename of await fs.promises.readdir(cwd)) {
-    if (Ruleset.isDefaultRulesetFile(filename)) {
+    if (DEFAULT_RULESET_FILE.test(filename)) {
       return path.join(cwd, filename);
     }
   }
@@ -19,17 +25,40 @@ async function getDefaultRulesetFile(): Promise<Optional<string>> {
 export async function getRuleset(rulesetFile: Optional<string>): Promise<Ruleset> {
   if (rulesetFile === void 0) {
     rulesetFile = await getDefaultRulesetFile();
-  } else if (!isAbsolute(rulesetFile)) {
+  } else if (!path.isAbsolute(rulesetFile)) {
     rulesetFile = path.join(process.cwd(), rulesetFile);
   }
 
   if (rulesetFile === void 0) {
-    return new Ruleset({ rules: {} });
+    throw new Error('No ruleset has been provided');
   }
 
-  const ruleset = (await import(rulesetFile)) as { default: RulesetDefinition } | RulesetDefinition;
+  let ruleset;
 
-  return new Ruleset('default' in ruleset ? ruleset.default : ruleset, {
+  if (/(json|ya?ml)$/.test(extname(rulesetFile))) {
+    const m: { exports?: RulesetDefinition } = {};
+    const paths = [path.dirname(rulesetFile)];
+
+    await AsyncFunction(
+      'module, require',
+      await migrateRuleset(rulesetFile, {
+        cwd: process.cwd(),
+        format: 'commonjs',
+        fs,
+      }),
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+    )(m, (id: string) => require(require.resolve(id, { paths })) as unknown);
+
+    ruleset = m.exports;
+  } else {
+    const imported = (await import(rulesetFile)) as { default: unknown } | unknown;
+    ruleset =
+      typeof imported === 'object' && imported !== null && 'default' in imported
+        ? (imported as Record<'default', unknown>).default
+        : imported;
+  }
+
+  return new Ruleset(ruleset, {
     severity: 'recommended',
     source: rulesetFile,
   });
