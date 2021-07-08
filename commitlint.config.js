082f25f1bc3500aff055053f3b6ceab158667855
Jakub Rożek <jakub@rozek.tech>
2021-07-08 12:45:22 +0200

    chore(repo): include deps-dev in scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const scopes = ['repo', 'test-harness', ...fs.readdirSync(path.join(__dirname, './packages'))];
+const scopes = ['repo', 'test-harness', 'deps-dev', ...fs.readdirSync(path.join(__dirname, './packages'))];
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
