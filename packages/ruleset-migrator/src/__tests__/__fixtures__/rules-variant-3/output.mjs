ee8b3506905a29b37184564b826a0a6677299303
Jakub Rożek <jakub@stoplight.io>
2021-08-24 13:05:18 +0200

    fix(ruleset-migrator): occasionally broken length function import (#1784)

---
+++ packages/ruleset-migrator/src/__tests__/__fixtures__/rules-variant-3/output.mjs
@@ -0,0 +1,17 @@
+import { truthy, length as length$0 } from '@stoplight/spectral-functions';
+export default {
+  rules: {
+    rule: {
+      given: '$',
+      then: {
+        function: truthy,
+      },
+    },
+    'valid-length': {
+      given: '$',
+      then: {
+        function: length$0,
+      },
+    },
+  },
+};
