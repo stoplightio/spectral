d140e91fd4fb4d5d4a91f65406db71ec4fa048b2
Jakub Rożek <jakub@stoplight.io>
2021-09-14 11:28:14 +0200

    chore(core): remove inline-version.mjs script (#1830)

--- packages/core/src/consts.ts
+++ packages/core/src/consts.ts
@@ -1,8 +1,6 @@
 import { DiagnosticSeverity } from '@stoplight/types';
 import { ParserOptions } from './ruleset';
 
-export const SPECTRAL_PKG_VERSION = '';
-
 export const DEFAULT_PARSER_OPTIONS = Object.freeze<Required<ParserOptions>>({
   incompatibleValues: DiagnosticSeverity.Error,
   duplicateKeys: DiagnosticSeverity.Error,
