953951e30171c65b0c10b51802f94a95620dbf5a
Jakub Rożek <jakub@stoplight.io>
2021-10-29 15:11:10 +0300

    build(repo): validate commit messages (#1731)
    * build(repo): validate commit messages
    
    * chore(repo): include deps-dev in scopes
    
    * chore(repo): handle merge commits
    
    * chore(repo): parents might be null

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,13 @@
 /*eslint-env node*/
+const fs = require('fs');
+const path = require('path');
+
+const scopes = ['repo', 'test-harness', 'deps-dev', ...fs.readdirSync(path.join(__dirname, './packages'))];
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-empty': [2, 'never'],
+    'scope-enum': [2, 'always', scopes],
+  },
 };
