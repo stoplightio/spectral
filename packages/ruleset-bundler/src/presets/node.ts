5b0cf6a7455a57f589ccaf4cc4756591c4ddd029
Jakub Rożek <jakub@stoplight.io>
2021-12-06 18:55:15 +0200

    revert(ruleset-bundler): skip builtins as well

--- packages/ruleset-bundler/src/presets/node.ts
+++ packages/ruleset-bundler/src/presets/node.ts
@@ -1,5 +1,4 @@
 import { url } from '../plugins/url';
-import { builtins } from '../plugins/builtins';
 import type { PluginsPreset } from './types';
 
-export const node: PluginsPreset = io => [builtins(), url(io)];
+export const node: PluginsPreset = io => [url(io)];
