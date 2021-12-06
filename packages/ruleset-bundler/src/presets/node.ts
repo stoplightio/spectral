b11ed8b41e8db61c8338cfb25476582b18b90711
Jakub Rożek <jakub@stoplight.io>
2021-12-06 18:50:26 +0200

    revert(ruleset-bundler): skip commonjs in bundler for now

--- packages/ruleset-bundler/src/presets/node.ts
+++ packages/ruleset-bundler/src/presets/node.ts
@@ -1,11 +1,5 @@
-import * as commonjs from '@rollup/plugin-commonjs';
-
 import { url } from '../plugins/url';
 import { builtins } from '../plugins/builtins';
 import type { PluginsPreset } from './types';
 
-export const node: PluginsPreset = io => [
-  builtins(),
-  (commonjs as unknown as typeof import('@rollup/plugin-commonjs').default)(),
-  url(io),
-];
+export const node: PluginsPreset = io => [builtins(), url(io)];
