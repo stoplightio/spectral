94802f6d4650654b3d25aa48f58a1b5811f40121
Jakub Jankowski <jakubj@stoplight.io>
2021-12-14 14:06:36 +0100

    chore(core): export under different name

--- packages/core/src/ruleset/index.ts
+++ packages/core/src/ruleset/index.ts
@@ -1,6 +1,6 @@
 export { assertValidRuleset, RulesetValidationError } from './validation';
 export { getDiagnosticSeverity } from './utils';
-export { createRulesetFunction, SchemaDefinition } from './rulesetFunction';
+export { createRulesetFunction, SchemaDefinition as RulesetFunctionSchemaDefinition } from './rulesetFunction';
 export { Format } from './format';
 export { RulesetDefinition, RuleDefinition, ParserOptions, HumanReadableDiagnosticSeverity } from './types';
 export { Ruleset } from './ruleset';
