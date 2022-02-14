181d7be00cf0d67b7196a6448ade6a0eef9b4531
Jakub Rożek <jakub@stoplight.io>
2022-02-14 14:28:44 +0200

    chore(repo): make it possible run test-harness on Windows (#1171)

--- test-harness/spawn.ts
+++ test-harness/spawn.ts
@@ -1,8 +1,9 @@
-import { join } from '@stoplight/path';
 import { Optional } from '@stoplight/types';
 import * as child_process from 'child_process';
 import { Transform } from 'stream';
-import { normalizeLineEndings } from './helpers';
+import Shell = require('node-powershell');
+import { normalizeLineEndings, IS_WINDOWS } from './helpers';
+import { isError } from 'lodash';
 
 export type SpawnReturn = {
   stdout: string;
@@ -16,15 +17,15 @@ export type SpawnFn = (
   cwd: Optional<string>,
 ) => Promise<SpawnReturn>;
 
-const createStream = () =>
+const createStream = (): Transform =>
   new Transform({
-    transform(chunk, encoding, done) {
+    transform(chunk, encoding, done): void {
       this.push(chunk);
       done();
     },
   });
 
-function stringifyStream(stream: Transform) {
+function stringifyStream(stream: Transform): Promise<string> {
   let result = '';
 
   stream.on('readable', () => {
@@ -44,7 +45,51 @@ function stringifyStream(stream: Transform) {
   });
 }
 
-export const spawnNode: SpawnFn = async (script, env, cwd = join(__dirname, 'scenarios')) => {
+const r = /(.*)LASTEXITCODE=(.*)/s;
+
+export const spawnPowershell: SpawnFn = async (command, env, cwd): Promise<SpawnReturn> => {
+  const ps = new Shell({
+    executionPolicy: 'Bypass',
+    noProfile: true,
+    inputEncoding: 'utf8',
+    outputEncoding: 'utf8',
+  });
+
+  const winCommand = command.replace(/\/binaries\/(spectral\.exe|spectral)/, '/binaries/spectral.exe');
+  const wrappedCommand = `cd '${cwd}';${winCommand};echo LASTEXITCODE=$LASTEXITCODE`;
+  const finalCommand = `powershell -Command "& { ${wrappedCommand} }"`;
+
+  await ps.addCommand(finalCommand);
+
+  try {
+    const stdOut = await ps.invoke();
+    const splitted = r.exec(stdOut);
+
+    if (splitted === null) {
+      throw new Error('No LASTEXITCODE has been found in the output.');
+    }
+
+    return {
+      stderr: '',
+      stdout: normalizeLineEndings(splitted[1].trim()),
+      status: Number.parseInt(splitted[2]),
+    };
+  } catch (err) {
+    return {
+      stderr: normalizeLineEndings(isError(err) ? err.message.replace(r, '$1').trim() : String(err)),
+      stdout: '',
+      status: 1,
+    };
+  } finally {
+    await ps.dispose();
+  }
+};
+
+export const spawnNode: SpawnFn = async (script, env, cwd) => {
+  if (IS_WINDOWS) {
+    return spawnPowershell(script, env, cwd);
+  }
+
   const stderr = createStream();
   const stdout = createStream();
 
