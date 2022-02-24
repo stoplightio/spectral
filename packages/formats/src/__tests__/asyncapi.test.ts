b0b008d65794df177dbfe7d9589c90d541c2794d
Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>
2022-02-24 16:52:35 +0100

    feat(formats): support 2.1.0, 2.2.0, 2.3.0 AsyncAPI versions (#2067)

--- packages/formats/src/__tests__/asyncapi.test.ts
+++ packages/formats/src/__tests__/asyncapi.test.ts
@@ -1,10 +1,13 @@
-import { asyncApi2 } from '../asyncapi';
+import { aas2, aas2_0, aas2_1, aas2_2, aas2_3 } from '../asyncapi';
 
-describe('AsyncApi format', () => {
-  describe('AsyncApi 2.{minor}.{patch}', () => {
-    it.each([['2.0.17'], ['2.9.0'], ['2.9.3']])('recognizes %s version correctly', (version: string) => {
-      expect(asyncApi2({ asyncapi: version }, null)).toBe(true);
-    });
+describe('AsyncAPI format', () => {
+  describe('AsyncAPI 2.x', () => {
+    it.each(['2.0.0', '2.1.0', '2.2.0', '2.3.0', '2.0.17', '2.1.37', '2.9.0', '2.9.3'])(
+      'recognizes %s version correctly',
+      version => {
+        expect(aas2({ asyncapi: version }, null)).toBe(true);
+      },
+    );
 
     const testCases = [
       { asyncapi: '3.0' },
@@ -15,6 +18,7 @@ describe('AsyncApi format', () => {
       { asyncapi: '2.0.01' },
       { asyncapi: '1.0' },
       { asyncapi: 2 },
+      { asyncapi: null },
       { openapi: '4.0' },
       { openapi: '2.0' },
       { openapi: null },
@@ -25,7 +29,50 @@ describe('AsyncApi format', () => {
     ];
 
     it.each(testCases)('does not recognize invalid document %o', document => {
-      expect(asyncApi2(document, null)).toBe(false);
+      expect(aas2(document, null)).toBe(false);
     });
+  });
+
+  describe('AsyncAPI 2.0', () => {
+    it.each(['2.0.0', '2.0.3'])('recognizes %s version correctly', version => {
+      expect(aas2_0({ asyncapi: version }, null)).toBe(true);
+    });
+
+    it.each(['2', '2.0', '2.1.0', '2.1.3'])('does not recognize %s version', version => {
+      expect(aas2_0({ asyncapi: version }, null)).toBe(false);
+    });
+  });
+
+  describe('AsyncAPI 2.1', () => {
+    it.each(['2.1.0', '2.1.37'])('recognizes %s version correctly', version => {
+      expect(aas2_1({ asyncapi: version }, null)).toBe(true);
+    });
+
+    it.each(['2', '2.1', '2.0.0', '2.2.0', '2.2.3'])('does not recognize %s version', version => {
+      expect(aas2_1({ asyncapi: version }, null)).toBe(false);
+    });
+  });
+
+  describe('AsyncAPI 2.2', () => {
+    it.each(['2.2.0', '2.2.3'])('recognizes %s version correctly', version => {
+      expect(aas2_2({ asyncapi: version }, null)).toBe(true);
+    });
+
+    it.each(['2', '2.2', '2.0.0', '2.1.0', '2.1.37', '2.3.0', '2.3.3'])('does not recognize %s version', version => {
+      expect(aas2_2({ asyncapi: version }, null)).toBe(false);
+    });
+  });
+
+  describe('AsyncAPI 2.3', () => {
+    it.each(['2.3.0', '2.3.3'])('recognizes %s version correctly', version => {
+      expect(aas2_3({ asyncapi: version }, null)).toBe(true);
+    });
+
+    it.each(['2', '2.3', '2.0.0', '2.1.0', '2.1.37', '2.2.0', '2.4.0', '2.4.3'])(
+      'does not recognize %s version',
+      version => {
+        expect(aas2_3({ asyncapi: version }, null)).toBe(false);
+      },
+    );
   });
 });
