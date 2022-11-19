6f95789a9aa2f52077f8763e4b770f284d0c3657
Jakub Rożek <jakub@rozek.tech>
2022-11-19 12:58:02 +0100

    fix(core): more accurate ruleset error paths

--- packages/core/src/ruleset/validation/__tests__/validation.test.ts
+++ packages/core/src/ruleset/validation/__tests__/validation.test.ts
@@ -761,7 +761,12 @@ describe('JS Ruleset Validation', () => {
         }),
       ).toThrowAggregateError(
         new AggregateError([
-          new RulesetValidationError('undefined-function', 'Function is not defined', ['rules', 'rule', 'then']),
+          new RulesetValidationError('undefined-function', 'Function is not defined', [
+            'rules',
+            'rule',
+            'then',
+            'function',
+          ]),
         ]),
       );
     });
