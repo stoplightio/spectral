84afbef32f14a8d05bed670bccaae8538ccce91f
Matatjahu <urbanczyk.maciej.95@gmail.com>
2023-02-01 18:09:34 +0100

    feat(rulesets): support AsyncAPI 2.6.0

--- packages/rulesets/src/asyncapi/functions/utils/specs.ts
+++ packages/rulesets/src/asyncapi/functions/utils/specs.ts
@@ -1,21 +1,6 @@
-// import only 2.X.X AsyncAPI JSON Schemas for better treeshaking
-import * as asyncAPI2_0_0Schema from '@asyncapi/specs/schemas/2.0.0.json';
-import * as asyncAPI2_1_0Schema from '@asyncapi/specs/schemas/2.1.0.json';
-import * as asyncAPI2_2_0Schema from '@asyncapi/specs/schemas/2.2.0.json';
-import * as asyncAPI2_3_0Schema from '@asyncapi/specs/schemas/2.3.0.json';
-import * as asyncAPI2_4_0Schema from '@asyncapi/specs/schemas/2.4.0.json';
-import * as asyncAPI2_5_0Schema from '@asyncapi/specs/schemas/2.5.0.json';
+import specs from '@asyncapi/specs';
 
 export type AsyncAPISpecVersion = keyof typeof specs;
-
-export const specs = {
-  '2.0.0': asyncAPI2_0_0Schema,
-  '2.1.0': asyncAPI2_1_0Schema,
-  '2.2.0': asyncAPI2_2_0Schema,
-  '2.3.0': asyncAPI2_3_0Schema,
-  '2.4.0': asyncAPI2_4_0Schema,
-  '2.5.0': asyncAPI2_5_0Schema,
-};
 
 const versions = Object.keys(specs);
 export const latestVersion = versions[versions.length - 1];
