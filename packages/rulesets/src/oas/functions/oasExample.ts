ff5b1cf833442662c8eeba898bf26a3e6a3c6c06
Phil Adams <phil_adams@us.ibm.com>
2023-02-21 19:09:31 -0600

    fix(rulesets): avoid false errors from ajv
    This commit modifies the oasExample function so
    that example fields are removed from the schema
    to be used for validation.  This is needed because
    the presence of an "example" field in a schema
    confuses ajv in certain scenarios.
    References:
    - https://github.com/stoplightio/spectral/issues/2081
    - https://github.com/stoplightio/spectral/issues/2140
    - https://github.com/ajv-validator/ajv/issues/1426

--- packages/rulesets/src/oas/functions/oasExample.ts
+++ packages/rulesets/src/oas/functions/oasExample.ts
@@ -110,6 +110,54 @@ function* getSchemaValidationItems(
   }
 }
 
+/**
+ * Modifies 'schema' (and all its sub-schemas) to remove all "example" fields.
+ * In this context, "sub-schemas" refers to all schemas reachable from 'schema'
+ * (e.g. properties, additionalProperties, allOf/anyOf/oneOf, not, items, etc.)
+ * @param schema the schema to be "de-examplified"
+ * @returns 'schema' with example fields removed
+ */
+function deExamplify(schema: Record<string, unknown>): Record<string, unknown> {
+  if ('items' in schema && typeof schema.items === 'object') {
+    schema.items = deExamplify(schema.items as Record<string, unknown>);
+  }
+
+  if (Array.isArray(schema.allOf)) {
+    for (let i = 0; i < schema.allOf.length; i++) {
+      schema.allOf[i] = deExamplify(schema.allOf[i] as Record<string, unknown>);
+    }
+  }
+  if (Array.isArray(schema.anyOf)) {
+    for (let i = 0; i < schema.anyOf.length; i++) {
+      schema.anyOf[i] = deExamplify(schema.anyOf[i] as Record<string, unknown>);
+    }
+  }
+  if (Array.isArray(schema.oneOf)) {
+    for (let i = 0; i < schema.oneOf.length; i++) {
+      schema.oneOf[i] = deExamplify(schema.oneOf[i] as Record<string, unknown>);
+    }
+  }
+  if ('not' in schema && typeof schema.not === 'object') {
+    schema.not = deExamplify(schema.not as Record<string, unknown>);
+  }
+
+  if ('additionalProperties' in schema && typeof schema.additionalProperties === 'object') {
+    schema.additionalProperties = deExamplify(schema.additionalProperties as Record<string, unknown>);
+  }
+
+  if ('properties' in schema && typeof schema.properties === 'object') {
+    for (const propName in schema.properties) {
+      schema.properties[propName] = deExamplify(schema.properties[propName] as Record<string, unknown>);
+    }
+  }
+
+  if ('example' in schema) {
+    delete schema.example;
+  }
+
+  return schema;
+}
+
 export default createRulesetFunction<Record<string, unknown>, Options>(
   {
     input: {
@@ -148,6 +196,12 @@ export default createRulesetFunction<Record<string, unknown>, Options>(
       schemaOpts.schema = { ...schemaOpts.schema };
       delete schemaOpts.schema.required;
     }
+
+    // Make a deep copy of the schema and then remove all the "example" fields from it.
+    // This is to avoid problems down in "ajv" which does the actual schema validation.
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    schemaOpts.schema = JSON.parse(JSON.stringify(schemaOpts.schema));
+    schemaOpts.schema = deExamplify(schemaOpts.schema);
 
     for (const validationItem of validationItems) {
       const result = oasSchema(validationItem.value, schemaOpts, {
