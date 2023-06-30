deab7a6b75d4bb9ed58ec98fa4f0efa0d145b9f4
Jakub Rożek <jakub@stoplight.io>
2023-06-30 14:03:12 +0200

    fix(core): invalid then produced by Rule#toJSON (#2496)

--- packages/core/src/ruleset/index.ts
+++ packages/core/src/ruleset/index.ts
@@ -4,4 +4,5 @@ export { createRulesetFunction, SchemaDefinition as RulesetFunctionSchemaDefinit
 export { Format } from './format';
 export { RulesetDefinition, RuleDefinition, ParserOptions, HumanReadableDiagnosticSeverity } from './types';
 export { Ruleset, StringifiedRuleset } from './ruleset';
+export { Formats } from './formats';
 export { Rule, StringifiedRule } from './rule';
