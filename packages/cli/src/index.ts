056f2e1eef966b807734fb00aae071411feabe75
Jakub Rożek <jakub@stoplight.io>
2023-08-04 10:23:50 +0200

    fix(cli): choose proxy agent based on requester protocol (#2521)

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -6,12 +6,12 @@ import { DEFAULT_REQUEST_OPTIONS } from '@stoplight/spectral-runtime';
 import lintCommand from './commands/lint';
 
 if (typeof process.env.PROXY === 'string') {
-  const { protocol } = new URL(process.env.PROXY);
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const { HttpProxyAgent, HttpsProxyAgent } = require('hpagent') as typeof import('hpagent');
-  DEFAULT_REQUEST_OPTIONS.agent = new (protocol === 'https:' ? HttpsProxyAgent : HttpProxyAgent)({
-    proxy: process.env.PROXY,
-  });
+  const httpAgent = new HttpProxyAgent({ proxy: process.env.PROXY });
+  const httpsAgent = new HttpsProxyAgent({ proxy: process.env.PROXY });
+
+  DEFAULT_REQUEST_OPTIONS.agent = url => (url.protocol === 'http:' ? httpAgent : httpsAgent);
 }
 
 export default yargs
