91fdded2fcb25880f4ec40091d7d8137f9b64504
Jakub Rożek <jakub@stoplight.io>
2024-03-22 13:37:03 +0100

    fix(parsers): update @stoplight/yaml from ~4.2.3 to ~4.3.0

--- packages/parsers/src/yaml.ts
+++ packages/parsers/src/yaml.ts
@@ -1,13 +1,21 @@
-import { getLocationForJsonPath, parseWithPointers, trapAccess, YamlParserResult } from '@stoplight/yaml';
-import { IParser } from './types';
+import { getLocationForJsonPath as _getLocationForJsonPath, parseWithPointers, trapAccess } from '@stoplight/yaml';
+import type { YamlParserResult as _YamlParserResult } from '@stoplight/yaml';
+import type { ILocation, JsonPath } from '@stoplight/types';
 
-export { YamlParserResult };
+import type { IParser } from './types';
+
+export type YamlParserResult<T> = Omit<_YamlParserResult<T>, 'comments'>;
+
+function getLocationForJsonPath<T>(result: YamlParserResult<T>, path: JsonPath): ILocation | undefined {
+  return _getLocationForJsonPath(result as _YamlParserResult<T>, path);
+}
 
 export const parseYaml = (input: string): YamlParserResult<unknown> =>
   parseWithPointers(input, {
     ignoreDuplicateKeys: false,
     mergeKeys: true,
     preserveKeyOrder: true,
+    attachComments: false,
   });
 
 export const Yaml: IParser<YamlParserResult<unknown>> = {
