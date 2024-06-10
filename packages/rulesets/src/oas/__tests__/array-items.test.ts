162d5f779594f3146f9839cce37266b9ec0fda9f
Vazha Omanashvili <rainum@users.noreply.github.com>
2024-06-10 13:22:24 +0300

    fix(rulesets): fixed array-items type property selector (#2638)
    Co-authored-by: Vazha Omanashvili <vazha.omanasvhili@smartbear.com>

--- packages/rulesets/src/oas/__tests__/array-items.test.ts
+++ packages/rulesets/src/oas/__tests__/array-items.test.ts
@@ -26,7 +26,41 @@ testRule('array-items', [
   },
 
   {
-    name: 'array items sibling is present',
+    name: 'array items sibling is present in a oas2 document',
+    document: {
+      swagger: '2.0',
+      securityDefinitions: {
+        apikey: {},
+        $ref: '#/securityDefinitions/apikey',
+      },
+      paths: {
+        $ref: '#/securityDefinitions/apikey',
+        '/path': {
+          get: {
+            '200': {
+              schema: {
+                type: 'array',
+                items: {},
+              },
+            },
+          },
+          post: {
+            '201': {
+              type: 'array',
+              items: {
+                type: 'array',
+                items: {},
+              },
+            },
+          },
+        },
+      },
+    },
+    errors: [],
+  },
+
+  {
+    name: 'array items sibling is present in oas3 document',
     document: {
       $ref: '#/',
       responses: {
@@ -46,8 +80,85 @@ testRule('array-items', [
     },
     errors: [],
   },
+
   {
-    name: 'array items sibling is missing',
+    name: 'array items sibling is present in oas3.1 document',
+    document: {
+      openapi: '3.1.0',
+      paths: {
+        '/path': {
+          get: {
+            responses: {
+              '200': {
+                type: 'array',
+                items: {},
+              },
+            },
+          },
+          post: {
+            responses: {
+              '201': {
+                type: 'array',
+                items: {
+                  type: 'array',
+                  items: {},
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    errors: [],
+  },
+
+  {
+    name: 'array items sibling is missing in a oas2 document',
+    document: {
+      swagger: '2.0',
+      securityDefinitions: {
+        apikey: {},
+        $ref: '#/securityDefinitions/apikey',
+      },
+      paths: {
+        $ref: '#/securityDefinitions/apikey',
+        '/path': {
+          get: {
+            '200': {
+              schema: {
+                type: 'array',
+              },
+            },
+          },
+          post: {
+            '201': {
+              type: 'array',
+              items: {
+                type: 'array',
+              },
+            },
+          },
+        },
+      },
+    },
+    errors: [
+      {
+        code: 'array-items',
+        message: 'Schemas with "type: array", require a sibling "items" field',
+        path: ['paths', '/path', 'get', '200', 'schema'],
+        severity: DiagnosticSeverity.Error,
+      },
+      {
+        code: 'array-items',
+        message: 'Schemas with "type: array", require a sibling "items" field',
+        path: ['paths', '/path', 'post', '201', 'items'],
+        severity: DiagnosticSeverity.Error,
+      },
+    ],
+  },
+
+  {
+    name: 'array items sibling is missing in oas3 document',
     document: {
       $ref: '#/',
       responses: {
@@ -74,6 +185,48 @@ testRule('array-items', [
         code: 'array-items',
         message: 'Schemas with "type: array", require a sibling "items" field',
         path: ['responses', '201', 'items'],
+        severity: DiagnosticSeverity.Error,
+      },
+    ],
+  },
+
+  {
+    name: 'array items sibling is missing in oas3.1 document',
+    document: {
+      openapi: '3.1.0',
+      paths: {
+        '/path': {
+          get: {
+            responses: {
+              '200': {
+                type: 'array',
+              },
+            },
+          },
+          post: {
+            responses: {
+              '201': {
+                type: 'array',
+                items: {
+                  type: 'array',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    errors: [
+      {
+        code: 'array-items',
+        message: 'Schemas with "type: array", require a sibling "items" field',
+        path: ['paths', '/path', 'get', 'responses', '200'],
+        severity: DiagnosticSeverity.Error,
+      },
+      {
+        code: 'array-items',
+        message: 'Schemas with "type: array", require a sibling "items" field',
+        path: ['paths', '/path', 'post', 'responses', '201', 'items'],
         severity: DiagnosticSeverity.Error,
       },
     ],
