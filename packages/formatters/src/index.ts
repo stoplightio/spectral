04bf901e653fdc08c48103ef66984080f928309d
Jean-Baptiste Muscat <muscatjb@gmail.com>
2024-09-13 09:22:35 +0200

    feat(formatters): add markdown formatter (#2662)
    * feat(formatters): add markdown formatter
    
    - Format results as a markdown table
    - New utility function getDocumentationUrl.ts that builds the documentation's url
    from the rule or the ruleset
    
    * feat(cli): add markdown output to the cli
    
    * chore(deps): use custom markdown table generator
    
    markdown-table-ts is not compatible with node12
    
    ---------
    
    Co-authored-by: jb.muscat <jb.muscat@criteo.com>
    Co-authored-by: Nauman <mnaumanali94@gmail.com>

--- packages/formatters/src/index.ts
+++ packages/formatters/src/index.ts
@@ -4,6 +4,7 @@ export * from './junit';
 export * from './html';
 export * from './text';
 export * from './teamcity';
+export * from './markdown';
 import type { Formatter } from './types';
 export type { Formatter, FormatterOptions } from './types';
 
