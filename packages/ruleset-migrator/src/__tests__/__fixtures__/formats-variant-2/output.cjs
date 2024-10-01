53ad1ec4e688a05b9d85d3c58da9cecdad4d4471
jonaslagoni <jonas-lt@live.dk>
2024-10-01 08:24:35 +0200

    chore(rulesets): update dependencies

--- packages/ruleset-migrator/src/__tests__/__fixtures__/formats-variant-2/output.cjs
+++ packages/ruleset-migrator/src/__tests__/__fixtures__/formats-variant-2/output.cjs
@@ -1,11 +1,11 @@
-const { asyncapi2, jsonSchemaLoose, oas2, oas3, oas3_0, oas3_1 } = require('@stoplight/spectral-formats');
+const { asyncapi2, asyncapi3, jsonSchemaLoose, oas2, oas3, oas3_0, oas3_1 } = require('@stoplight/spectral-formats');
 const { truthy } = require('@stoplight/spectral-functions');
 module.exports = {
   formats: [oas2, oas3_1, oas3_0, jsonSchemaLoose],
   rules: {
     test: {
       given: '$',
-      formats: [asyncapi2, oas3, oas3_0, oas3_1],
+      formats: [asyncapi2, asyncapi3, oas3, oas3_0, oas3_1],
       then: {
         function: truthy,
       },
