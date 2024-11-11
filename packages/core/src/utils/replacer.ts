174661a283885b3d0b5395d1a151877cddf9f1c7
Nauman Ali <mnaumanali94@gmail.com>
2024-11-11 19:25:58 +0500

    fix(core): make simplae compatible with newer node versions

--- packages/core/src/utils/replacer.ts
+++ packages/core/src/utils/replacer.ts
@@ -1,11 +1,13 @@
-import { Dictionary } from '@stoplight/types';
-import reduce from 'simple-eval';
+// @ts-expect-error: no types
+import parse from 'nimma/parser';
+// @ts-expect-error: needs new ts resolution
+import reduce from 'simple-eval/eval';
 
 export type Transformer<V = Record<string, unknown>> = (this: V, ...args: unknown[]) => string;
 
 export class Replacer<V extends Record<string, unknown>> {
   protected readonly regex: RegExp;
-  protected readonly functions: Dictionary<Transformer<V>>;
+  protected readonly functions: Record<string, Transformer<V>>;
 
   constructor(count: number) {
     this.regex = new RegExp(`#?${'{'.repeat(count)}([^}\n]+)${'}'.repeat(count)}`, 'g');
@@ -18,14 +20,13 @@ export class Replacer<V extends Record<string, unknown>> {
   }
 
   public print(input: string, values: V): string {
-    return input.replace(this.regex, (substr, identifier, index) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    return input.replace(this.regex, (substr, identifier: string, index: number) => {
       const shouldEvaluate = input[index] === '#';
 
       if (shouldEvaluate) {
         return String(
-          reduce(identifier, {
-            ...Object.entries(this.functions).reduce((fns, [name, fn]) => {
+          simpleEval(identifier, {
+            ...Object.entries(this.functions).reduce<Record<string, Transformer<V>>>((fns, [name, fn]) => {
               fns[name] = fn.bind(values);
               return fns;
             }, {}),
@@ -38,8 +39,12 @@ export class Replacer<V extends Record<string, unknown>> {
         return '';
       }
 
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       return String(values[identifier]);
     });
   }
+}
+
+function simpleEval(expression: string, ctx: Record<string, unknown>): unknown {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  return reduce(parse(`$[?(${expression})]`)[0].value, ctx);
 }
