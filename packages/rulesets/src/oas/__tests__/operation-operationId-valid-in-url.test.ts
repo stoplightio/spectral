2b895a97d74cc35e4fd1d99beb4c2b57befda568
Sebastian Legarraga <64795732+slegarraga@users.noreply.github.com>
2026-08-08 16:56:35 -0400

    fix(rulesets): clarify operationId URL validation message
    Signed-off-by: Sebastian Legarraga <64795732+slegarraga@users.noreply.github.com>

--- packages/rulesets/src/oas/__tests__/operation-operationId-valid-in-url.test.ts
+++ packages/rulesets/src/oas/__tests__/operation-operationId-valid-in-url.test.ts
@@ -31,7 +31,7 @@ testRule('operation-operationId-valid-in-url', [
     },
     errors: [
       {
-        message: 'operationId must not characters that are invalid when used in URL.',
+        message: 'operationId must not contain characters that are invalid when used in URL.',
         path: ['paths', '/todos', 'get', 'operationId'],
         severity: DiagnosticSeverity.Warning,
       },
