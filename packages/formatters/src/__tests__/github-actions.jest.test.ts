4163ea35003296f802b4b5f104cfddd72cc6ffc7
Charles Treatman <ctreatman@equinix.com>
2026-09-11 12:05:24 -0500

    fix(formatters): include ruleset-based documentationUrl in github format
    The GitHub Actions formatter includes the documentationUrl for a rule if
    it is defined, but some rules, such as the ones in the built-in oas ruleset,
    rely on a ruleset-level documentationUrl.
    
    This updates the GitHub Actions formatter to use the existing getDocumentationUrl
    resolver so that the documentationUrl will be included in GitHub Actions annotations
    whenever it is available.

--- packages/formatters/src/__tests__/github-actions.jest.test.ts
+++ packages/formatters/src/__tests__/github-actions.jest.test.ts
@@ -37,15 +37,63 @@ const results: IRuleResult[] = [
         character: 60,
       },
     },
-    documentationUrl: 'https://example.com/pets',
+  },
+  {
+    code: 'operation-summary',
+    message: 'paths./pets.get.summary is too long',
+    path: ['paths', '/pets', 'get', 'summary'],
+    severity: 1,
+    source: `${cwd}/__tests__/fixtures/petstore.oas2.yaml`,
+    range: {
+      start: {
+        line: 60,
+        character: 8,
+      },
+      end: {
+        line: 71,
+        character: 60,
+      },
+    },
   },
 ];
 
+const context = {
+  ruleset: {
+    rules: {
+      'operation-description': {
+        documentationUrl: '', // nothing
+        owner: {
+          definition: {
+            documentationUrl: '', // nothing
+          },
+        },
+      },
+      'operation-tags': {
+        documentationUrl: '', //nothing
+        owner: {
+          definition: {
+            documentationUrl: 'https://ruleset-documentation-url.com',
+          },
+        },
+      },
+      'operation-summary': {
+        documentationUrl: 'https://rule-documentation-url.com',
+        owner: {
+          definition: {
+            documentationUrl: 'https://ruleset-documentation-url.com',
+          },
+        },
+      },
+    },
+  },
+} as unknown as FormatterContext;
+
 describe('GitHub Actions formatter', () => {
   test('should be formatted correctly', () => {
-    expect(githubActions(results, { failSeverity: DiagnosticSeverity.Error }).split('\n')).toEqual([
+    expect(githubActions(results, { failSeverity: DiagnosticSeverity.Error }, context).split('\n')).toEqual([
       '::warning title=operation-description,file=__tests__/fixtures/petstore.oas2.yaml,col=9,endColumn=61,line=61,endLine=72::paths./pets.get.description is not truthy%0AMessage can have%0Amultiple lines',
-      '::warning title=operation-tags,file=__tests__/fixtures/petstore.oas2.yaml,col=9,endColumn=61,line=61,endLine=72::paths./pets.get.tags is not truthy%0ADocumentation: https://example.com/pets',
+      '::warning title=operation-tags,file=__tests__/fixtures/petstore.oas2.yaml,col=9,endColumn=61,line=61,endLine=72::paths./pets.get.tags is not truthy%0ADocumentation: https://ruleset-documentation-url.com#operation-tags',
+      '::warning title=operation-summary,file=__tests__/fixtures/petstore.oas2.yaml,col=9,endColumn=61,line=61,endLine=72::paths./pets.get.summary is too long%0ADocumentation: https://rule-documentation-url.com',
     ]);
   });
 });
